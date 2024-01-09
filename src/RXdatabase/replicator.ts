a37e828373ead0610bf14e9d4a6b6a7d41ea5f71
Daniel <carreraprogrammer@gmail.com>
2024-01-09 12:00:54 -0500

    Add GraphQLReplicator class for database replication

---
+++ src/RXdatabase/replicator.ts
@@ -0,0 +1,59 @@
+import { RxDatabase } from "rxdb";
+import { replicateGraphQL } from "rxdb/plugins/replication-graphql";
+import { AppDataBase } from "./database";
+
+export class GraphQLReplicator {
+  private database: AppDataBase;
+  private syncURL = {
+    http: `${process.env.URL_PATH}:${process.env.GRAPHQL_PORT}/${process.env.GRAPHQL_PATH}`,
+  }
+
+  private replicationStates: any[] = [];
+
+  constructor(database: AppDataBase) {
+    this.database = database;
+
+    this.startReplication
+  }
+
+  public startReplication() {
+    const collections = this.database.getCollections()
+
+    collections.forEach((collection) => this.setupGraphQLReplication(collection))
+
+  }
+
+  public stopReplication() {
+    this.replicationStates.forEach(replicationState => {
+      if (replicationState) {
+        replicationState.cancel();
+      }
+    });
+    this.replicationStates = [];
+  }
+
+  private setupGraphQLReplication(collection: any) {
+    const replicationState = replicateGraphQL({
+      collection: collection,
+      url: this.syncURL,
+      pull: {
+        queryBuilder: collection.schema.getPullQueryBuilder(),
+        batchSize: collection.schema.batchSize,
+      },
+      push: {
+        queryBuilder: collection.schema.getPushQueryBuilder(),
+        batchSize: collection.schema.batchSize,
+      },
+      deletedField: 'deleted',
+      live: true,
+      replicationIdentifier: `replication-${collection.name}`
+    });
+
+    replicationState.error$.subscribe((err) => {
+      console.error(`Replication error in ${collection.name}:`);
+      console.dir(err);
+    });
+
+    return replicationState;
+  }
+}
