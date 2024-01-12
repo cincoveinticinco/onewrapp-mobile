82c06d113a4d69d997b4d48299905b988ca21320
Daniel <carreraprogrammer@gmail.com>
2024-01-11 20:20:20 -0500

    Create general toolbar component and add AddScene route

--- src/components/Shared/Toolbar.tsx
+++ src/components/Shared/Toolbar.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IonToolbar, IonButton, IonIcon, IonTitle } from '@ionic/react';
-import { menu } from 'ionicons/icons';
+import { add, ellipsisHorizontal, funnel, menu, search } from 'ionicons/icons';
 import './Toolbar.css';
 
 interface ToolbarProps {
-  name: string
+  name: string,
 }
 
-const Toolbar:React.FC<ToolbarProps> = ({ name }) => (
-  <IonToolbar color="primary" className="toolbar">
-    <IonButton slot="start" fill="clear" color="light" className="toolbar-burger-menu">
-      <IonIcon icon={menu} color="light" />
-    </IonButton>
-    <IonTitle className='toolbar-title' slot="start">{name}</IonTitle>
-</IonToolbar>
-);
-
+const Toolbar:React.FC<ToolbarProps> = ({ name }) =>{
+  return (
+    <IonToolbar color="primary" className="toolbar">
+      <IonButton slot="start" fill="clear" className="toolbar-burger-menu">
+        <IonIcon icon={menu} className='toolbar-icon' />
+      </IonButton>
+      <IonTitle className='toolbar-title' slot="start">{name}</IonTitle>
+      <IonButton slot="end">
+        <IonIcon icon={search} className="toolbar-search-icon toolbar-icon" />
+      </IonButton>
+      <IonButton slot="end" routerLink={'addscene'} >
+        <IonIcon icon={add} className="toolbar-add-icon toolbar-icon" />
+      </IonButton>
+      <IonButton slot="end">
+        <IonIcon icon={funnel} className="toolbar-filter-icon toolbar-icon" />
+      </IonButton>
+      <IonButton slot="end">
+        <IonIcon icon={ellipsisHorizontal} className="toolbar-ellipsis-icon toolbar-icon" />
+      </IonButton>
+  </IonToolbar>
+  );
+}
 export default Toolbar;
