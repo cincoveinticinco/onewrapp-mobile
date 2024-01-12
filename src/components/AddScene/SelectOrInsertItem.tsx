3e8f91549a51196e4b64cd6514f7e3d2123bc3b7
Daniel <carreraprogrammer@gmail.com>
2024-01-12 11:54:18 -0500

    Add SelectOrInsertItem component

---
+++ src/components/AddScene/SelectOrInsertItem.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { IonItem, IonLabel, IonSelect, IonSelectOption, IonInput } from '@ionic/react';
+
+interface SelectOrInsertItemProps {
+  label: string;
+  selectValue: any;
+  inputPlaceholder?: string;
+  onInputChange: (event: CustomEvent) => void;
+  onSelectChange: (event: CustomEvent) => void;
+  options: Array<{ label: string; value: any }>;
+}
+
+const SelectOrInsertItem: React.FC<SelectOrInsertItemProps> = ({
+  label,
+  selectValue,
+  inputPlaceholder,
+  onInputChange,
+  onSelectChange,
+  options
+}) => {
+  const [isInsertMode, setIsInsertMode] = useState(selectValue === 'INSERT');
+
+  const handleSelectChange = (event: CustomEvent) => {
+    setIsInsertMode(event.detail.value === 'INSERT');
+    onSelectChange(event);
+  };
+
+  return (
+    <>
+      <IonItem>
+        <IonLabel position='stacked'>{label}</IonLabel>
+        <IonSelect value={isInsertMode ? 'INSERT' : selectValue} onIonChange={handleSelectChange}>
+          {options.map((option, index) => (
+            <IonSelectOption key={index} value={option.value}>{option.label}</IonSelectOption>
+          ))}
+          <IonSelectOption value="INSERT">Insert</IonSelectOption>
+        </IonSelect>
+      </IonItem>
+      {isInsertMode && (
+        <IonItem>
+          <IonInput clearInput={true} placeholder={inputPlaceholder} onIonChange={onInputChange} />
+        </IonItem>
+      )}
+    </>
+  );
+};
+
+export default SelectOrInsertItem;
