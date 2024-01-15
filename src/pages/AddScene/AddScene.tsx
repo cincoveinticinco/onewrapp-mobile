893cf66a9a369186b37e3a4110af44b00a6cff95
Daniel <carreraprogrammer@gmail.com>
2024-01-15 16:26:44 -0500

    Add mobile support and back button functionality to AddScene page

--- src/pages/AddScene/AddScene.tsx
+++ src/pages/AddScene/AddScene.tsx
@@ -1,9 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './AddScene.css';
 import { useEffect } from 'react';
 import AddScenesForm from '../../components/AddScene/AddSceneForm';
+import { useHistory } from 'react-router';
+import { useIsMobile } from '../../hooks/useIsMobile';
+import { chevronBack } from 'ionicons/icons';
 
 const AddScene = () => {
+
+  const history = useHistory();
+  const isMobile = useIsMobile();
+
   useEffect(() => {
     const tabsContainer: any = document.querySelector('.app-tabs-container');
     if (tabsContainer) {
@@ -16,15 +23,39 @@ const AddScene = () => {
       }
     };
   }, []);
+
+  const handleBack = () => {
+    history.goBack();
+  };
   
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color='dark'>
-          <IonTitle> Add Strip </IonTitle>
+        <IonToolbar color='tertiary' className='add-strip-toolbar'>
+          {
+          !isMobile &&
+          <>
+            <IonButton fill='clear' color="primary" slot='start' onClick={handleBack}>
+              CANCEL
+            </IonButton>
+            <IonButton fill='clear' color="primary" slot='end'>
+              SAVE
+            </IonButton>
+          </>
+          }
+
+          {
+            isMobile && 
+            <IonButton fill='clear' color="primary" slot='start' onClick={handleBack}>
+              <IonIcon  icon={chevronBack} color='light'  />
+            </IonButton>
+          }
+          <IonTitle className='add-strip-toolbar-title'> 
+            Add Strip 
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent color='dark'>
+      <IonContent color='tertiary'>
         <AddScenesForm />
       </IonContent>
     </IonPage>
