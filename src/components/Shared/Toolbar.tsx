6d77ab8312cc411457708ae9e19072d20411e9f2
Daniel <carreraprogrammer@gmail.com>
2024-01-15 16:26:11 -0500

    Add border-bottom to toolbar and adjust button sizes

--- src/components/Shared/Toolbar.tsx
+++ src/components/Shared/Toolbar.tsx
@@ -1,31 +1,51 @@
 import React, { useEffect } from 'react';
 import { IonToolbar, IonButton, IonIcon, IonTitle } from '@ionic/react';
-import { add, ellipsisHorizontal, funnel, menu, search } from 'ionicons/icons';
+import { add, addOutline, ellipsisHorizontal, ellipsisHorizontalOutline, funnel, funnelOutline, menu, menuOutline, search, searchOutline } from 'ionicons/icons';
 import './Toolbar.css';
 
 interface ToolbarProps {
   name: string,
+  search?: boolean,
+  addScene?: boolean,
+  filter?: boolean,
+  elipse?: boolean,
 }
 
-const Toolbar:React.FC<ToolbarProps> = ({ name }) =>{
+const Toolbar:React.FC<ToolbarProps> = ({ name, search, addScene, filter, elipse}) =>{
   return (
-    <IonToolbar color="primary" className="toolbar">
-      <IonButton slot="start" fill="clear" className="toolbar-burger-menu">
-        <IonIcon icon={menu} className='toolbar-icon' />
+    <IonToolbar color="tertiary" className="toolbar">
+      <IonButton slot="start" fill="clear" className="toolbar-burger-menu ion-no-padding">
+        <IonIcon icon={menuOutline} className='toolbar-icon' />
       </IonButton>
-      <IonTitle className='toolbar-title' slot="start">{name}</IonTitle>
-      <IonButton slot="end">
-        <IonIcon icon={search} className="toolbar-search-icon toolbar-icon" />
-      </IonButton>
-      <IonButton slot="end" routerLink={'addscene'} >
-        <IonIcon icon={add} className="toolbar-add-icon toolbar-icon" />
-      </IonButton>
-      <IonButton slot="end">
-        <IonIcon icon={funnel} className="toolbar-filter-icon toolbar-icon" />
-      </IonButton>
-      <IonButton slot="end">
-        <IonIcon icon={ellipsisHorizontal} className="toolbar-ellipsis-icon toolbar-icon" />
-      </IonButton>
+      <IonTitle className='toolbar-title ion-no-padding' slot="start">{name}</IonTitle>
+      {
+        search 
+        &&
+        <IonButton fill="clear" slot="end" className='ion-no-padding toolbar-button'>
+          <IonIcon icon={searchOutline} className="toolbar-search-icon toolbar-icon" />
+        </IonButton>
+      }
+      {
+        addScene 
+        &&
+        <IonButton fill="clear" slot="end" routerLink={'addscene'} className='ion-no-padding toolbar-button'>
+          <IonIcon icon={addOutline} className="toolbar-add-icon toolbar-icon" />
+        </IonButton>
+      }
+      {
+        filter
+        &&
+        <IonButton fill="clear" slot="end" className='ion-no-padding toolbar-button'>
+          <IonIcon icon={funnelOutline} className="toolbar-filter-icon toolbar-icon" />
+        </IonButton>
+      }
+      {
+        elipse
+        &&
+        <IonButton fill="clear" slot="end" className='ion-no-padding toolbar-button'>
+          <IonIcon icon={ellipsisHorizontalOutline} className="toolbar-ellipsis-icon toolbar-icon" />
+        </IonButton>
+      }
   </IonToolbar>
   );
 }
