f92b862b2ccd88205192ba3ba8735b13bcfffc17
Daniel <carreraprogrammer@gmail.com>
2024-01-14 23:31:13 -0500

    Add AddCharacterInput component and change AddCategoryForm

--- src/components/AddScene/AddCharacterForm.tsx
+++ src/components/AddScene/AddCharacterForm.tsx
@@ -1,73 +1,96 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Character } from '../../interfaces/scenesTypes';
-import { IonInput, IonItem, IonList, IonButton, IonIcon } from '@ionic/react';
-import { trash } from 'ionicons/icons';
+import { useState } from 'react';
+import { IonItem, IonButton, IonIcon, IonAlert, IonGrid, IonCard, IonHeader, IonTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonInput } from '@ionic/react';
+import { add, trash } from 'ionicons/icons';
+import AddCharacterInput from './AddCharacterInput';
 
-interface AddCharacterFormProps {
-  categoryName: string;
-  showForm: boolean;
-  setShowForm: (showForm: boolean) => void;
+interface AddCategoryFormProps {
   handleSceneChange: (value: any, field: string) => void;
 }
 
-const AddCharacterForm: React.FC<AddCharacterFormProps> = ({ categoryName, showForm, setShowForm, handleSceneChange}) => {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const characterNameInputRef = useRef<HTMLIonInputElement>(null);
+const AddCharacterForm: React.FC<AddCategoryFormProps> = ( { handleSceneChange }) => {
 
-  useEffect(() => {
-    handleSceneChange(characters, 'characters');
-  }, [characters])
-
-  const deleteCharacter = (characterNum: number) => {
-    setCharacters(currentCharacters => 
-      currentCharacters.filter(character => character.characterNum !== characterNum)
-    );
+  const [categories, setCategories] = useState<string[]>([]);
+  
+  const toggleForm = (index: number) => {
+    const element = document.getElementById(`character-form-${index}`);
+    if (element) {
+     element.style.display === 'none' ? element.style.display = 'block' : element.style.display = 'none';
+    }
   };
 
-  const addCharacter = () => {
-    const newCharacterName = characterNameInputRef.current?.value as string;
-    if (!newCharacterName) return; // Do nothing if the input is empty
-
-    const newCharacter = {
-      categoryName: categoryName,
-      characterName: newCharacterName,
-      characterNum: characters.length + 1,
-    };
-
-    setCharacters(currentCharacters => [...currentCharacters, newCharacter]);
-    setShowForm(false);
-
-    if (characterNameInputRef.current) {
-      characterNameInputRef.current.value = '';
+  const handleOk = (inputData: { categoryName: string; }) => {
+    const inputElement = document.getElementById('add-category-input');
+    if (inputData.categoryName) {
+      setCategories([...categories, inputData.categoryName]);
     }
+    if (inputElement) {
+      (inputElement as HTMLInputElement).value = '';
+    }
+  };
+
+  const removeCategory = (categoryName: string) => {
+    const updatedCategories = categories.filter(category => category !== categoryName);
+    setCategories(updatedCategories);
   };
 
   return (
     <>
-      {characters.length > 0 && (
-        <IonList>
-          {characters.map((character, index) => (
-            <IonItem key={index}>
-              {character.characterName}
-              <IonButton slot='end' onClick={() => deleteCharacter(character.characterNum)}>
-                <IonIcon icon={trash} />
-              </IonButton>
-            </IonItem>
+      <IonItem>
+        Characters
+        <IonButton id="category-alert" slot='end'>
+          <IonIcon icon={add} />
+        </IonButton>
+      </IonItem>
+      <IonAlert
+        trigger='category-alert'
+        header='Please, enter a category name'
+        buttons={[
+          {
+            text: 'OK',
+            handler: handleOk
+          }
+        ]}
+        inputs={[
+          {
+            name: 'categoryName',
+            type: 'text',
+            placeholder: 'Category Name',
+            id: 'add-category-input'
+          }
+        ]}
+      ></IonAlert>
+
+      {categories.length > 0 && 
+        <IonGrid>
+          {categories.map((category, index) => (
+            <IonCard key={index}>
+              <IonCardHeader className='ion-flex'>
+                <div className='ion-flex ion-justify-content-between'>
+                  <IonCardSubtitle className='ion-flex ion-align-items-center'>
+                    {category}
+                  </IonCardSubtitle>
+                  <IonButton 
+                    size='small' 
+                    onClick={() => {toggleForm(index)}}
+                  >
+                    <IonIcon icon={add} />
+                  </IonButton>
+                </div>
+                <IonCardContent>
+                  <AddCharacterInput
+                    categoryName={category} 
+                    toggleForm={toggleForm}
+                    id={index}
+                    handleSceneChange={handleSceneChange}
+                  />
+                </IonCardContent>
+              </IonCardHeader>
+            </IonCard>  
           ))}
-        </IonList>
-      )}
-      {showForm && (
-        <IonItem>
-          <IonInput 
-            placeholder='Character Name' 
-            ref={characterNameInputRef}
-            clearInput={true}
-          />
-          <IonButton onClick={addCharacter}>Add Character</IonButton>
-        </IonItem>
-      )}
+        </IonGrid>
+      }
     </>
   );
-}
+};
 
 export default AddCharacterForm;
---
+++ src/components/AddScene/AddCharacterInput.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Character } from '../../interfaces/scenesTypes';
+import { IonInput, IonItem, IonList, IonButton, IonIcon } from '@ionic/react';
+import { trash } from 'ionicons/icons';
+import { v4 as uuid } from 'uuid';
+
+interface AddCharacterInputProps {
+  categoryName: string;
+  toggleForm: (index: number) => void;
+  handleSceneChange: (value: any, field: string) => void;
+  id: number;
+}
+
+const AddCharacterInput: React.FC<AddCharacterInputProps> = ({ categoryName, toggleForm, handleSceneChange, id}) => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const characterNameInputRef = useRef<HTMLIonInputElement>(null);
+
+  useEffect(() => {
+    handleSceneChange(characters, 'characters');
+  }, [characters])
+
+  const deleteCharacter = (characterNum: number) => {
+    setCharacters(currentCharacters => 
+      currentCharacters.filter(character => character.characterNum !== characterNum)
+    );
+  };
+
+  const addCharacter = () => {
+    const newCharacterName = characterNameInputRef.current?.value as string;
+    if (!newCharacterName) return; 
+
+    const newCharacter = {
+      categoryName: categoryName,
+      characterName: newCharacterName,
+      characterNum: characters.length + 1,
+    };
+
+    setCharacters(currentCharacters => [...currentCharacters, newCharacter]);
+    toggleForm(id)
+
+    if (characterNameInputRef.current) {
+      characterNameInputRef.current.value = '';
+    }
+  };
+
+  return (
+    <>
+      {characters.length > 0 && (
+        <IonList>
+          {characters.map((character, index) => (
+            <IonItem
+              key={`character-item-${index}`}
+            >
+              {character.characterName}
+              <IonButton slot='end' onClick={() => deleteCharacter(character.characterNum)}>
+                <IonIcon icon={trash} />
+              </IonButton>
+            </IonItem>
+          ))}
+        </IonList>
+      )}
+        <IonItem
+          style={{ display: 'none'}}
+          id={`character-form-${id}`}
+        >
+          <IonInput
+            placeholder='Character Name' 
+            ref={characterNameInputRef}
+            clearInput={true}
+          />
+          <IonButton onClick={addCharacter}>Add Character</IonButton>
+        </IonItem>
+    </>
+  );
+}
+
+export default AddCharacterInput;
