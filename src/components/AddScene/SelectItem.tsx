bab88bdd3fa00ed39b73fe978a0d9a07d54acca7
Daniel <carreraprogrammer@gmail.com>
2024-01-15 16:26:02 -0500

    Add inputName prop to SelectItem component

--- src/components/AddScene/SelectItem.tsx
+++ src/components/AddScene/SelectItem.tsx
@@ -7,12 +7,13 @@ interface SelectItemProps {
   onChange: (event: CustomEvent) => void;
   options: Array<{ label: string; value: any }>;
   disabled?: boolean;
+  inputName: string;
 }
 
-const SelectItem: React.FC<SelectItemProps> = ({ label, value, onChange, options, disabled }) => {
+const SelectItem: React.FC<SelectItemProps> = ({ label, value, onChange, options, disabled, inputName }) => {
   return (
-    <IonItem>
-      <IonSelect label={label} labelPlacement='stacked' interface="popover" value={value} onIonChange={onChange} disabled={disabled}>
+    <IonItem color="tertiary" id={inputName}>
+      <IonSelect placeholder="SELECT TYPE" label={label} labelPlacement='stacked' interface="popover" value={value} onIonChange={onChange} disabled={disabled} >
         {options.map((option, index) => (
           <IonSelectOption key={`select-${option}-${index}`} value={option.value}>{option.label}</IonSelectOption>
         ))}
