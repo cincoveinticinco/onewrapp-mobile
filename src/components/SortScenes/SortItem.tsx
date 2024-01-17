51633b3eb55257cbb19f624efb7eda1d5c26cf9f
Daniel <carreraprogrammer@gmail.com>
2024-01-17 09:19:08 -0500

    Add SortItem component

---
+++ src/components/SortScenes/SortItem.tsx
@@ -0,0 +1,33 @@
+import { IonButton, IonCheckbox, IonIcon, IonItem } from '@ionic/react'
+import { appsSharp } from 'ionicons/icons'
+import React from 'react'
+import './SortItem.scss'
+
+interface SortItemProps {
+  sortOption: any;
+}
+
+const SortItem: React.FC<SortItemProps> = ({ sortOption }) => {
+  return (
+    <IonItem color="tertiary" className='sort-option-item'>
+      <IonButton fill='clear' color="light" slot='start' className='sort-page-icons'>
+        <IonIcon className="ion-no-padding ion-no-margin" icon={appsSharp} />
+      </IonButton>
+      <IonCheckbox
+        className='sort-option-checkbox'
+        slot="start"
+        labelPlacement='end'
+      >
+        {sortOption.label}
+      </IonCheckbox>
+      <IonButton fill="clear" color="light" slot="end">
+        DESC
+      </IonButton>
+      <IonButton fill="clear" color="light" slot="end">
+        ASC
+      </IonButton>
+    </IonItem>
+  )
+}
+
+export default SortItem
