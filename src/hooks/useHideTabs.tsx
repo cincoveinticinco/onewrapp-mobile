7b91c5a4c0e6281dc9f99afddf99ff1f62dac863
Daniel <carreraprogrammer@gmail.com>
2024-01-17 11:55:17 -0500

    Create useHideTabs hook to hide tabs

---
+++ src/hooks/useHideTabs.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+const useHideTabs = () => {
+  useEffect(() => {
+    const element = document.querySelector('.app-tabs-container');
+    if (element instanceof HTMLElement) {
+      element.style.display = 'none';
+    }
+
+    return () => {
+      if (element) {
+        if (element instanceof HTMLElement) {
+          element.style.display = '';
+        }
+      }
+    };
+  }, []);
+}
+
+export default useHideTabs;
