fa0345a55c3324d74bb16a03a34e387a488df3b4
Daniel <carreraprogrammer@gmail.com>
2024-01-20 23:50:28 -0500

    Add ScenesFiltersContext component

---
+++ src/context/scenesFiltersContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+const ScenesFiltersContext = createContext({});
+
+export const ScenesFiltersProvider = ({ children }: { children: React.ReactNode }) => {
+
+  const [filterOptions, setFilterOptions] = useState({});
+
+  const contextValue = {
+    filterOptions,
+    setFilterOptions
+  };
+
+  return (
+    <ScenesFiltersContext.Provider value={contextValue}>
+      {children}
+    </ScenesFiltersContext.Provider>
+  );
+};
+
+export default ScenesFiltersContext;
