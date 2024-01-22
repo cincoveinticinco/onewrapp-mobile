08cd8f20c43424b80e374c34ea241f854e03f6b2
Daniel <carreraprogrammer@gmail.com>
2024-01-21 19:07:16 -0500

    Add Projects page and related components and contexts

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,6 @@ import { Redirect, Route } from 'react-router';
 import { IonReactRouter } from '@ionic/react-router';
 import { AuthContext } from './context/auth';
 import LoginPage from './pages/LoginPage/LoginPage';
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
 import Projects from './pages/Projects/Projects';
 import AppTabs from './components/Shared/AppTabs';
 import { AppDataBase } from './RXdatabase/database';
@@ -34,6 +15,18 @@ import { ProjectsSchema } from './RXdatabase/schemas/projects';
 import { ScenesSchema } from './RXdatabase/schemas/scenes';
 import { DatabaseContext } from './context/database';
 import { ScenesFiltersProvider } from './context/scenesFiltersContext';
+
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import './theme/variables.css';
 
 setupIonicReact();
 
