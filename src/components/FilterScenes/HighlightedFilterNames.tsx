bf2464cf2111aa5549458aedf6628b7a7e259319
Daniel <carreraprogrammer@gmail.com>
2024-01-24 21:00:37 -0500

    Add HighlightedFilterNames component to highlight filter options with numeration

---
+++ src/components/FilterScenes/HighlightedFilterNames.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface HighlightedFilterOptionProps {
+  option: string;
+  checked: () => boolean;
+}
+
+const HighlightedFilterNames: React.FC<HighlightedFilterOptionProps> = ({ option, checked }) => {
+  const startsWithNumberAndDot = /^[0-9]+\./.test(option);
+
+  const defineColor = (isChecked: boolean) => {
+    if (isChecked) {
+      return { color: 'var(--ion-color-yellow)' };
+    } else {
+      return {};
+    }
+  }
+  
+  if (startsWithNumberAndDot) {
+    // If the option starts with a number and a dot, it will be highlighted in red
+    const numberAndDotPart = option.match(/^[0-9]+\./)![0];
+    const restPart = option.replace(numberAndDotPart, '');
+    
+    return (
+      <p>
+        <span style={{ color: 'var(--ion-color-danger)' }}>{numberAndDotPart}</span>
+        <span style={defineColor(checked())}>{restPart.toUpperCase()}</span>
+      </p>
+    );
+  } else {
+    // If the option does not start with a number and a dot, it will be highlighted in blue
+    return <p style={defineColor(checked())}>{option.toUpperCase()}</p>;
+  }
+};
+
+export default HighlightedFilterNames;
