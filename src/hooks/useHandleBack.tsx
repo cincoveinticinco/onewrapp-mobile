edb5a49cef19127ee22bbe64c47216b3623a1fc4
Daniel <carreraprogrammer@gmail.com>
2024-01-24 21:07:06 -0500

    Add useHandleBack hook for handling back navigation

---
+++ src/hooks/useHandleBack.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useHistory } from 'react-router'
+
+const useHandleBack = () => {
+  const history = useHistory()
+
+  const handleBack = () => {
+    history.goBack()
+  }
+
+  return handleBack
+}
+
+export default useHandleBack
