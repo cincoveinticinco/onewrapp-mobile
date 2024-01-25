9476ff3ba89f1afaaa712eefb2995e09ed9fb250
Daniel <carreraprogrammer@gmail.com>
2024-01-24 21:01:01 -0500

    Add OutlineLightButton component for reusable outlineLightButton

---
+++ src/components/Shared/OutlineLightButton.tsx
@@ -0,0 +1,22 @@
+import { IonButton } from '@ionic/react'
+import React from 'react'
+
+interface OutlineLightButtonProps {
+  buttonName: string,
+  onClick: () => void
+  className?: string
+}
+
+const OutlineLightButton: React.FC<OutlineLightButtonProps> = ({buttonName, onClick, className}) => {
+  return (
+  <IonButton
+    expand='block' 
+    onClick={onClick}
+    className={'outline-light-button ' + className }
+  >
+    {buttonName}
+  </IonButton>
+  )
+}
+
+export default OutlineLightButton
