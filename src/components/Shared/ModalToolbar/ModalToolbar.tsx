e5332c133adedd7ee44971814d8f3e847d1095e2
Daniel <carreraprogrammer@gmail.com>
2024-02-01 12:47:34 -0500

    Create ModalToolbar reusabl component

---
+++ src/components/Shared/ModalToolbar/ModalToolbar.tsx
@@ -0,0 +1,68 @@
+import { IonButton, IonIcon, IonTitle, IonToolbar } from '@ionic/react'
+import React from 'react'
+import useIsMobile from '../../../hooks/useIsMobile'
+import { chevronBack, save } from 'ionicons/icons'
+import './ModalToolbar.scss'
+
+interface ModalToolbarProps {
+  clearOptions?: () => void
+  handleBack: () => void
+  toolbarTitle: string
+  saveOptions?: () => void
+}
+
+const ModalToolbar: React.FC<ModalToolbarProps> = (
+  {
+    clearOptions,
+    handleBack,
+    toolbarTitle,
+    saveOptions
+  }
+) => {
+  const isMobile = useIsMobile()
+
+  return (
+    <IonToolbar color="tertiary" id="modal-toolbar ion-no-padding">
+      {
+        !isMobile
+        && (
+          <>
+            <IonButton fill="clear" color="primary" slot="start" onClick={handleBack}>
+              BACK
+            </IonButton>
+            {
+              clearOptions && 
+              <IonButton
+                fill="clear"
+                color="primary"
+                slot="end"
+                onClick={clearOptions}
+              >
+                RESET
+              </IonButton>
+            }
+            {
+              saveOptions && 
+              <IonButton fill="clear" color="primary" slot="end" onClick={saveOptions}>
+                SAVE
+              </IonButton>}
+          </>
+        )
+      }
+
+      {
+        isMobile
+        && (
+          <IonButton fill="clear" color="primary" slot="start" onClick={handleBack}>
+            <IonIcon icon={chevronBack} color="light" />
+          </IonButton>
+        )
+      }
+      <IonTitle className="modal-toolbar-title">
+        {toolbarTitle.toUpperCase()}
+      </IonTitle>
+    </IonToolbar>
+  )
+}
+
+export default ModalToolbar
