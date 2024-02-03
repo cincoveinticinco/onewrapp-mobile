bcba22f4afef2da57f75d009c25026061e9da800
Daniel <carreraprogrammer@gmail.com>
2024-02-03 10:34:30 -0500

    Refactor AddButton component

--- src/components/Shared/AddButton/AddButton.tsx
+++ src/components/Shared/AddButton/AddButton.tsx
@@ -1,34 +1,32 @@
-import { IonButton, IonIcon } from '@ionic/react'
-import { add } from 'ionicons/icons'
-import React from 'react'
-import './AddButton.scss'
+import { IonButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import React from 'react';
+import './AddButton.scss';
 
 interface AddButtonProps {
   id?: string
   className?: string
   onClick?: () => void
-  size?: "small" | "default" | "large" | undefined
-  slot?: "start" | "end" | "icon-only" | undefined
+  size?: 'small' | 'default' | 'large' | undefined
+  slot?: 'start' | 'end' | 'icon-only' | undefined
 }
 
 const AddButton: React.FC<AddButtonProps> = ({
   id,
   className,
   onClick,
-  slot
-}) => {
-  return (
-    <IonButton
-      fill="clear"
-      color="light"
-      id={id}
-      className={className + " add-button ion-no-padding"}
-      onClick={onClick}
-      slot={slot}
-    >
-      <IonIcon className="add-button-icon" icon={add} />
-    </IonButton>
-  )
-}
+  slot,
+}) => (
+  <IonButton
+    fill="clear"
+    color="light"
+    id={id}
+    className={`${className} add-button ion-no-padding`}
+    onClick={onClick}
+    slot={slot}
+  >
+    <IonIcon className="add-button-icon" icon={add} />
+  </IonButton>
+);
 
-export default AddButton
+export default AddButton;
