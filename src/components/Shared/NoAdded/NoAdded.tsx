1029c478538b32acf6a891f7515b3fca7427561f
Daniel <carreraprogrammer@gmail.com>
2024-02-03 11:06:20 -0500

    Add NoAdded component and styles

---
+++ src/components/Shared/NoAdded/NoAdded.tsx
@@ -0,0 +1,12 @@
+import { IonItem } from '@ionic/react'
+import './NoAdded.scss'
+
+const NoAdded = () => {
+  return (
+    <p className='no-added-to-this-strip' color='tertiary'>
+      NO ADDED TO THIS STRIP
+    </p>
+  )
+}
+
+export default NoAdded
