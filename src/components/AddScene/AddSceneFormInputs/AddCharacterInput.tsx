5f96b82a3d7d581a6648ac1e466b826ca16e986f
Daniel <carreraprogrammer@gmail.com>
2024-02-03 10:31:51 -0500

    Refactor AddCharacterInput component for sort characters by category

--- src/components/AddScene/AddSceneFormInputs/AddCharacterInput.tsx
+++ src/components/AddScene/AddSceneFormInputs/AddCharacterInput.tsx
@@ -1,60 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import {
-  IonItem, IonList, IonButton, IonIcon, IonAlert,
+  IonCardContent,
+  IonItem,
+  IonList,
 } from '@ionic/react';
-import { trash } from 'ionicons/icons';
 import { Character } from '../../../interfaces/scenesTypes';
 import InputModal from '../../Shared/InputModal/InputModal';
 import getCharactersArray from '../../../utils/getCharactersArray';
 import customArraySort from '../../../utils/customArraySort';
 import sceneData from '../../../data/scn_data.json';
 import removeNumberAndDot from '../../../utils/removeNumberAndDot';
+import DeleteButton from '../../Shared/DeleteButton/DeleteButton';
+import applyFilters from '../../../utils/applyFilters';
+import getUniqueValuesFromNestedArray from '../../../utils/getUniqueValuesFromNestedArray';
+import NoAdded from '../../Shared/NoAdded/NoAdded';
 
 interface AddCharacterInputProps {
   categoryName: string | null;
-  handleSceneChange: (value: any, field: string) => void
+  handleSceneChange: (value: any, field: string) => void;
 }
 
 const AddCharacterInput: React.FC<AddCharacterInputProps> = ({
-  categoryName, handleSceneChange,
+  categoryName,
+  handleSceneChange,
 }) => {
   const [selectedCharacters, setSelectedCharacters] = useState<Character[]>([]);
-
   const { scenes } = sceneData;
 
   useEffect(() => {
-    console.log('SELECTED CHARACTERS', selectedCharacters);
     handleSceneChange(selectedCharacters, 'characters');
   }, [selectedCharacters]);
 
   const deleteCharacter = (characterNum: string | null) => {
     if (characterNum) {
-      setSelectedCharacters((currentCharacters) => currentCharacters.filter((character: Character) => character.characterNum !== characterNum));
+      const updatedCharacters = selectedCharacters.filter(
+        (character: Character) => character.characterNum !== characterNum
+      );
+      setSelectedCharacters(updatedCharacters);
     }
   };
 
-  const getSortedCharacterNames = customArraySort(getCharactersArray());
+  const uniqueCharacterValuesArray = getUniqueValuesFromNestedArray(
+    scenes,
+    'characters',
+    'characterName'
+  );
+
+  const categoryCriteria = categoryName === 'NO CATEGORY' ? null : categoryName;
+
+  const getFilteredCharacters = applyFilters(uniqueCharacterValuesArray, {
+    categoryName: [categoryCriteria],
+  });
+
+  const getSortedCharacterNames = customArraySort(
+    getCharactersArray(getFilteredCharacters)
+  );
 
   const toggleCharacters = (character: string) => {
-    const sceneWithCharacter = scenes.find((scene: any) => scene.characters.some((char: any) => char.characterName.toUpperCase() === removeNumberAndDot(character.toUpperCase())));
-  
-    const characterObject = sceneWithCharacter?.characters.find((char: any) => char.characterName.toUpperCase() === removeNumberAndDot(character.toUpperCase()));
-  
+    const sceneWithCharacter = scenes.find((scene: any) =>
+      scene.characters.some(
+        (char: any) =>
+          char.characterName.toUpperCase() ===
+          removeNumberAndDot(character.toUpperCase())
+      )
+    );
+
+    const characterObject = sceneWithCharacter?.characters.find(
+      (char: any) =>
+        char.characterName.toUpperCase() ===
+        removeNumberAndDot(character.toUpperCase())
+    );
+
     if (characterObject) {
-      const selectedCharacterObjectIndex = selectedCharacters.findIndex((char: any) => char.characterName === characterObject.characterName);
+      const selectedCharacterObjectIndex = selectedCharacters.findIndex(
+        (char: any) => char.characterName === characterObject.characterName
+      );
       if (selectedCharacterObjectIndex !== -1) {
-        setSelectedCharacters((currentCharacters) => currentCharacters.filter((char: any) => char.characterName !== characterObject.characterName));
+        setSelectedCharacters((currentCharacters) =>
+          currentCharacters.filter(
+            (char: any) => char.characterName !== characterObject.characterName
+          )
+        );
       } else {
         const newCharacter: any = { ...characterObject };
-        newCharacter.categoryName = newCharacter.categoryName !== 'NO CATEGORY' ? null : categoryName; 
-        setSelectedCharacters((currentCharacters) => [...currentCharacters, newCharacter]);
+        newCharacter.categoryName =
+          categoryName !== 'NO CATEGORY' ? categoryName : null;
+        setSelectedCharacters((currentCharacters) => [
+          ...currentCharacters,
+          newCharacter,
+        ]);
       }
     }
-  }
+  };
+
+  const contentStyle = selectedCharacters.length === 0 ? 'ion-no-padding' : '';
 
   return (
-    <>
-      {selectedCharacters.length > 0 && (
+    <IonCardContent className={contentStyle}>
+      {selectedCharacters.length > 0 ? (
         <IonList className="ion-no-padding ion-no-margin">
           {selectedCharacters.map((character, index) => (
             <IonItem
@@ -63,21 +106,26 @@ const AddCharacterInput: React.FC<AddCharacterInputProps> = ({
               className="ion-no-margin category-items"
             >
               {`${character.characterNum}. ${character.characterName}`}
-              <IonButton fill="clear" color="danger" slot="end" onClick={() => deleteCharacter(character.characterNum)}>
-                <IonIcon icon={trash} />
-              </IonButton>
+              <DeleteButton
+                onClick={() => deleteCharacter(character.characterNum)}
+                slot="end"
+              />
             </IonItem>
           ))}
         </IonList>
+      ) : (
+        <NoAdded />
       )}
       <InputModal
-        optionName='character'
+        optionName={`Characters (  ${categoryName}  )`}
         listOfOptions={getSortedCharacterNames}
-        modalTrigger='open-character-options-modal'
+        modalTrigger={`open-characters-options-modal-${categoryName}`}
         handleCheckboxToggle={toggleCharacters}
-        selectedOptions={selectedCharacters.map((character) => character.characterName)}
+        selectedOptions={selectedCharacters.map(
+          (character) => character.characterName
+        )}
       />
-    </>
+    </IonCardContent>
   );
 };
 
