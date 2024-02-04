36afa0243bf6933e81a593488c45a02bd8c8619c
Daniel <carreraprogrammer@gmail.com>
2024-02-03 21:50:55 -0500

    Add search functionality to Toolbar component

--- src/Layouts/MainPagesLayout/MainPagesLayout.tsx
+++ src/Layouts/MainPagesLayout/MainPagesLayout.tsx
@@ -1,18 +1,35 @@
 import { IonHeader, IonPage } from '@ionic/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Toolbar from '../../components/Shared/Toolbar/Toolbar';
+import useIsMobile from '../../hooks/useIsMobile';
 
 interface MainPagesLayoutProps {
   children: React.ReactNode
 }
 
-const MainPagesLayout: React.FC<MainPagesLayoutProps> = ({ children }) => (
-  <IonPage>
-    <IonHeader>
-      <Toolbar name="LVE-STRIPS" search addScene filter elipse sort />
-    </IonHeader>
-    {children}
-  </IonPage>
-);
+const MainPagesLayout: React.FC<MainPagesLayoutProps> = ({ children }) => {
+  
+  const [searchMode, setSearchMode] = React.useState(false);
+  const isMobile = useIsMobile();
+
+  return (
+    <IonPage>
+      <IonHeader>
+      <Toolbar 
+        name="LVE-STRIPS"
+        menu={!isMobile}
+        back={isMobile}
+        search 
+        addScene 
+        filter 
+        elipse 
+        sort 
+        searchMode={searchMode} 
+        setSearchMode={setSearchMode}
+      />
+      </IonHeader>
+      {children}
+    </IonPage>
+)};
 
 export default MainPagesLayout;
--- src/components/Shared/Toolbar/Toolbar.tsx
+++ src/components/Shared/Toolbar/Toolbar.tsx
@@ -1,77 +1,96 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-  IonToolbar, IonButton, IonIcon, IonTitle,
+  IonToolbar, IonButton, IonIcon, IonTitle, IonInput,
 } from '@ionic/react';
 import {
-  menuOutline, searchOutline, addOutline, funnelOutline, ellipsisHorizontalOutline, swapVerticalOutline,
+  menuOutline, searchOutline, addOutline, funnelOutline, ellipsisHorizontalOutline, swapVerticalOutline, chevronBack,
 } from 'ionicons/icons';
 import './Toolbar.scss';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../../context/auth';
+import useHandleBack from '../../../hooks/useHandleBack';
 
 interface ToolbarProps {
   name: string;
+  menu?: boolean;
+  back?: boolean;
   search?: boolean;
   addScene?: boolean;
   filter?: boolean;
   elipse?: boolean;
   sort?: boolean;
+  searchMode?: boolean;
+  setSearchMode?: (searchMode: boolean) => void;
 }
 
 const Toolbar: React.FC<ToolbarProps> = ({
   name,
+  menu,
+  back = false,
   search = false,
   addScene = false,
   filter = false,
   elipse = false,
   sort = false,
-}) => (
-  <IonToolbar color="tertiary" className="toolbar" id="main-pages-toolbar">
-    <IonButton slot="start" fill="clear" className="toolbar-button ion-no-padding">
-      <IonIcon icon={menuOutline} className="toolbar-icon" />
-    </IonButton>
-    <Link to="/my/projects" style={{ textDecoration: 'none', color: 'inherit' }}>
-      <IonTitle className="toolbar-title" slot="start">{name}</IonTitle>
-    </Link>
-    {
-      search
-      && (
-        <IonButton fill="clear" slot="end" className="ion-no-padding toolbar-button">
-          <IonIcon icon={searchOutline} className="toolbar-search-icon toolbar-icon" />
+  searchMode = false,
+  setSearchMode = () => {},
+}) => {
+
+  const toggleSearchMode = () => {
+    setSearchMode(!searchMode);
+  }
+
+  useEffect(() => {
+    console.log(searchMode)
+  }, [searchMode]);
+
+  const handleBack = useHandleBack()
+  
+  return (
+    <IonToolbar color="tertiary" className="toolbar" id="main-pages-toolbar">
+      {
+        menu && <IonButton slot="start" fill="clear" className="toolbar-button ion-no-padding">
+          <IonIcon icon={menuOutline} className="toolbar-icon" />
+        </IonButton>}
+        <Link to="/my/projects" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <IonTitle className="toolbar-title" slot="start">{name}</IonTitle>
+        </Link>
+      {
+        back && 
+      
+        <IonButton fill="clear" slot="start" className="ion-no-padding toolbar-button">
+          <IonIcon icon={chevronBack} className="toolbar-back-icon toolbar-icon" onClick={handleBack} />
         </IonButton>
-      )
-    }
-    {
-      addScene
-      && (
+      }
+      {search && (
+        <div slot="end" className={`ion-no-padding toolbar-search-wrapper ${searchMode ? 'search' : ''}`}>
+          <IonButton fill="clear" slot="end" className="ion-no-padding toolbar-button" onClick={toggleSearchMode}>
+            <IonIcon icon={searchOutline} className="toolbar-search-icon toolbar-icon"/>
+          </IonButton>
+          <IonInput className="toolbar-search-input" placeholder="" disabled={!searchMode}/>
+        </div>
+      )}
+      {addScene && (
         <IonButton fill="clear" slot="end" routerLink="addscene" className="ion-no-padding toolbar-button">
           <IonIcon icon={addOutline} className="toolbar-add-icon toolbar-icon" />
         </IonButton>
-      )
-    }
-    {
-      filter
-      && (
+      )}
+      {filter && (
         <IonButton fill="clear" slot="end" routerLink="/my/projects/01/strips/filters" className="ion-no-padding toolbar-button">
           <IonIcon icon={funnelOutline} className="toolbar-filter-icon toolbar-icon" />
         </IonButton>
-      )
-    }
-    {
-      sort
-      && (
+      )}
+      {sort && (
         <IonButton fill="clear" slot="end" routerLink="/my/projects/01/sortscenes" className="ion-no-padding toolbar-button">
           <IonIcon icon={swapVerticalOutline} className="toolbar-sort-icon toolbar-icon" />
         </IonButton>
-      )
-    }
-    {
-      elipse
-      && (
+      )}
+      {elipse && (
         <IonButton fill="clear" slot="end" className="ion-no-padding toolbar-button">
           <IonIcon icon={ellipsisHorizontalOutline} className="toolbar-ellipsis-icon toolbar-icon" />
         </IonButton>
-      )
-    }
-  </IonToolbar>
-);
+      )}
+    </IonToolbar>
+)};
+
 export default Toolbar;
