3ab578c1de71e965bd71271b3620c0de7b65c30e
Daniel <carreraprogrammer@gmail.com>
2024-02-08 21:17:48 -0500

    Refactor AddScene component and add scrollToTop functionality

--- src/pages/AddScene/AddScene.tsx
+++ src/pages/AddScene/AddScene.tsx
@@ -3,18 +3,25 @@ import './AddScene.css';
 import AddScenesForm from '../../components/AddScene/AddSceneForm';
 import useHideTabs from '../../hooks/useHideTabs';
 import SecondaryPagesLayout from '../../Layouts/SecondaryPagesLayout/SecondaryPagesLayout';
+import { useRef } from 'react';
 
 const AddScene = () => {
+  const contentRef = useRef<HTMLIonContentElement>(null);
+
   useHideTabs();
 
   const handleSave = () => {
     console.log('I am saving');
   };
 
+  const scrollToTop = () => {
+    contentRef.current?.scrollToTop();
+  }
+
   return (
-    <SecondaryPagesLayout saveOptions={handleSave} pageTitle='Add Scene'>
-      <IonContent color="tertiary">
-        <AddScenesForm />
+    <SecondaryPagesLayout saveOptions={handleSave} pageTitle="Add Scene">
+      <IonContent color="tertiary" ref={contentRef}>
+        <AddScenesForm scrollToTop={() => scrollToTop()} />
       </IonContent>
     </SecondaryPagesLayout>
   );
