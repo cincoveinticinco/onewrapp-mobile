89d86585e792a5f99e65d2baee882ab34e5c4b63
Daniel <carreraprogrammer@gmail.com>
2024-02-16 13:52:23 -0500

    Refactor database initialization and schema setup

--- src/RXdatabase/database.ts
+++ src/RXdatabase/database.ts
@@ -1,69 +1,54 @@
 import { addRxPlugin, createRxDatabase } from 'rxdb';
 import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
-import { RxDBMigrationPlugin } from 'rxdb/plugins/migration-schema';
-import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
-import { RxDBQueryBuilderPlugin } from 'rxdb/plugins/query-builder';
-import { RxDBLeaderElectionPlugin } from 'rxdb/plugins/leader-election';
-import { RxDBUpdatePlugin } from 'rxdb/plugins/update';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import DatabaseSchema from './database_schema';
 
-addRxPlugin(RxDBMigrationPlugin);
 addRxPlugin(RxDBDevModePlugin);
-addRxPlugin(RxDBQueryBuilderPlugin);
-addRxPlugin(RxDBUpdatePlugin);
-addRxPlugin(RxDBLeaderElectionPlugin);
 
 export default class AppDataBase {
     private dbName = 'onewrappdb'
-
-    private dbPassword = ''
-
     private dbInstance: Promise<any>;
-
     private schemaList;
 
     constructor(schemaList: DatabaseSchema[]) {
-      this.schemaList = schemaList;
-
-      this.dbInstance = this.initializeDatabase();
+        this.schemaList = schemaList;
+        this.dbInstance = this.initializeDatabase();
     }
 
     private async initializeDatabase() {
-      const storage = wrappedValidateAjvStorage({
-        storage: getRxStorageDexie(),
-      });
+        const storage = wrappedValidateAjvStorage({
+            storage: getRxStorageDexie(),
+        });
 
-      const dbInstence = await createRxDatabase({
-        name: this.dbName,
-        storage,
-        multiInstance: false,
-      });
+        const dbInstance = await createRxDatabase({
+            name: this.dbName,
+            storage,
+            multiInstance: false,
+        });
 
-      await this.setCollections(dbInstence);
-
-      return dbInstence;
+        await this.setCollections(dbInstance);
+        return dbInstance;
     }
 
     private async setCollections(database: any) {
-      const persistentStorage = await database;
+        const persistentStorage = await database;
+        const schemaObject: { [key: string]: any } = {}; // Add index signature
 
-      const schemaObject:any = {};
+        this.schemaList.forEach((schema) => {
+          schemaObject[schema.SchemaName()] = {
+            schema: schema.Schema(),
+          };
+        });
 
-      this.schemaList.forEach((schema) => {
-        schemaObject[schema.SchemaName()] = {
-          schema: schema.Schema(),
-        };
-      });
-
-      await persistentStorage.addCollections({ ...schemaObject });
+        await persistentStorage.addCollections({ ...schemaObject });
     }
 
     public async getDatabaseInstance() {
-      return this.dbInstance;
+        return this.dbInstance;
     }
 
     public getCollections() {
-      return this.schemaList;
+        return this.schemaList;
     }
 }
