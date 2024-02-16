06f975b35cc71817eb7821e5c5b01663a8e239aa
Daniel <carreraprogrammer@gmail.com>
2024-02-16 13:18:14 -0500

    Add seed data to database context provider

--- src/context/database.tsx
+++ src/context/database.tsx
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import AppDataBase from '../RXdatabase/database';
 import ScenesSchema from '../RXdatabase/schemas/scenes';
 import ProjectsSchema from '../RXdatabase/schemas/projects';
+import scenesSeeds from '../data/scn_data.json';
 
 const sceneCollection = new ScenesSchema();
 const projectCollection = new ProjectsSchema();
@@ -26,6 +27,8 @@ export const DatabaseContextProvider = ({ children }: { children: React.ReactNod
         const dbInstance = await RXdatabase.getDatabaseInstance();
         setOneWrapRXdatabase(dbInstance);
         console.log('Database instance:', dbInstance);
+
+        await loadSeeds(dbInstance);
       } catch (error) {
         console.error('Error al obtener la instancia de la base de datos:', error);
       }
@@ -45,6 +48,30 @@ export const DatabaseContextProvider = ({ children }: { children: React.ReactNod
       subscription?.unsubscribe();
     };
   }, [offlineScenes$]);
+
+  const loadSeeds = async (dbInstance: any) => {
+    try {
+      scenesSeeds.scenes.forEach(async (scene: any) => {
+        const sceneExists = await dbInstance.scenes.findOne({
+          selector: {
+            id: scene.id,
+          },
+        }).exec();
+
+        const updatedScene = {
+          ...scene,
+          updatedAt: new Date().toISOString(),
+        };
+
+        if(!sceneExists) {
+          await dbInstance.scenes.insert(updatedScene);
+        }
+      });
+      console.log('Seeds loaded successfully');
+    } catch (error) {
+      console.error('Error loading seeds:', error);
+    }
+  };
 
   return (
     <DatabaseContext.Provider value={{ oneWrapDb: oneWrapRXdatabase, offlineScenes }}>
