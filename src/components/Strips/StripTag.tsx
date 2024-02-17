0a81c621917a2a547b70a989471f5c1524340ae5
Daniel <carreraprogrammer@gmail.com>
2024-02-17 16:59:29 -0500

    Refactor StripTag component to improve filter option clearing

--- src/components/Strips/StripTag.tsx
+++ src/components/Strips/StripTag.tsx
@@ -1,7 +1,7 @@
 import { IonButton, IonIcon } from '@ionic/react';
 import React, { useContext } from 'react';
 import './StripTag.scss';
-import { close, closeCircleOutline } from 'ionicons/icons';
+import { close } from 'ionicons/icons';
 import ScenesContext from '../../context/ScenesContext';
 
 interface StripTagProps {
@@ -12,90 +12,47 @@ interface StripTagProps {
 const StripTag: React.FC<StripTagProps> = ({ tagKey, filterOption }) => {
   const { selectedFilterOptions, setSelectedFilterOptions } = useContext(ScenesContext);
 
-  function findKeyByValue(obj: any, value: any) {
-    return Object.keys(obj).find(key => obj[key].includes(value));
-  }
+  const clearOption = (filterTag: string, filterOptions: any) => {
+    const newFilterOptions = { ...filterOptions };
 
-  interface SelectedFilterOptionsInterface {
-    [key: string]: any[];
-  }
+    Object.keys(filterOptions).forEach((key) => {
+      const value = filterOptions[key];
 
-  const clearOption = (filterTag: string, filterOptions: SelectedFilterOptionsInterface) => {
-    const optionKey: any = findKeyByValue(filterOptions, filterTag)
-
-    Object.entries(filterOptions).forEach(([key, value]) => {
-      if (optionKey && filterOptions[optionKey].every((el) => typeof el === 'string')) {
-        if(value.length  <= 1) {
-          const newFilterOptions: SelectedFilterOptionsInterface = {
-            ...filterOptions
+      if (Array.isArray(value)) {
+        const index = value.indexOf(filterTag);
+        if (index !== -1) {
+          const newArray = [...value.slice(0, index), ...value.slice(index + 1)];
+          if (newArray.length === 0) {
+            delete newFilterOptions[key];
+          } else {
+            newFilterOptions[key] = newArray;
           }
-
-          delete newFilterOptions[optionKey]
-
-          setSelectedFilterOptions({
-            ...newFilterOptions
-          })
-        } else {
-          const filteredOptionArray = filterOptions[optionKey].filter((option) => {
-            return option !== filterTag
-          })
-
-          setSelectedFilterOptions({
-            ...filterOptions,
-            [optionKey]: filteredOptionArray
-          })
         }
-      } else if(value.every((val: any) => typeof val === 'object')) {
-        value.forEach((val: any) => {
-
-          const newSubArray = value.filter((option) => option !== val)
-
-          Object.entries(val).forEach(([subKey, subValue]: any[]) => {
-            console.log()
-            if(subValue.length <= 1) {
-
-              if(newSubArray.length <= 1) {
-                const newFilterOptions = {
-                  ...filterOptions
-                }
-
-                delete newFilterOptions[key]
-
-                setSelectedFilterOptions({
-                  ...newFilterOptions
-                })
-              }
+      } else if (typeof value === 'object') {
+        Object.keys(value).forEach((subKey) => {
+          const subValue = value[subKey];
+          if (Array.isArray(subValue) && subValue.includes(filterTag)) {
+            const subArray = subValue.filter((option: string) => option !== filterTag);
+            if (subArray.length === 0) {
+              delete value[subKey];
             } else {
-              const newSubArray = subValue.filter((option: string) => option !== filterTag)
-              const subValueIndex = value.findIndex(({key, array}) => array === subValue )
-              value[subValueIndex] = newSubArray
-
-              const newFilterOptions = {
-                ...filterOptions
-              }
-
-              const objectIndex = newFilterOptions[key].findIndex((option) => {
-                return option === val
-              })
-
-              newFilterOptions[key][objectIndex][subKey] = newSubArray
-
-              console.log('NEW FILTER OPTIONS',newFilterOptions)
-
-              setSelectedFilterOptions(
-                newFilterOptions
-              )
+              value[subKey] = subArray;
             }
-          })
-        })
+          }
+        });
+        if (Object.keys(value).length === 0) {
+          delete newFilterOptions[key];
+        }
       }
     });
+
+    setSelectedFilterOptions({ ...newFilterOptions });
   };
 
   return (
     <div key={tagKey} className="filter-tag-container">
-      <p className=' ion-no-padding ion-no-margin'>{filterOption.toUpperCase()}</p>
-      <IonButton onClick={() => clearOption(filterOption, (selectedFilterOptions as SelectedFilterOptionsInterface))} fill='clear' color='danger' className='ion-no-padding'>
+      <p className='ion-no-padding ion-no-margin'>{filterOption.toUpperCase()}</p>
+      <IonButton onClick={() => clearOption(filterOption, selectedFilterOptions)} fill='clear' color='danger' className='ion-no-padding'>
         <IonIcon icon={close} />
       </IonButton>
     </div>
