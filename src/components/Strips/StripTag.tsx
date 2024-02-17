4b635ed5398b232cafa5490a7697e293a187155a
Daniel <carreraprogrammer@gmail.com>
2024-02-17 16:02:18 -0500

    Fix clearing options in StripTag component

--- src/components/Strips/StripTag.tsx
+++ src/components/Strips/StripTag.tsx
@@ -1,8 +1,8 @@
-import { IonButton, IonIcon } from '@ionic/react';
 import React, { useContext } from 'react';
-import './StripTag.scss';
-import { close, closeCircleOutline } from 'ionicons/icons';
+import { IonButton, IonIcon } from '@ionic/react';
+import { close } from 'ionicons/icons';
 import ScenesContext from '../../context/ScenesContext';
+import './StripTag.scss';
 
 interface StripTagProps {
   tagKey: string;
@@ -12,90 +12,61 @@ interface StripTagProps {
 const StripTag: React.FC<StripTagProps> = ({ tagKey, filterOption }) => {
   const { selectedFilterOptions, setSelectedFilterOptions } = useContext(ScenesContext);
 
-  function findKeyByValue(obj: any, value: any) {
+  // Function to find key by value
+  const findKeyByValue = (obj: any, value: any) => {
     return Object.keys(obj).find(key => obj[key].includes(value));
-  }
+  };
 
-  interface SelectedFilterOptionsInterface {
-    [key: string]: any[];
-  }
-
-  const clearOption = (filterTag: string, filterOptions: SelectedFilterOptionsInterface) => {
-    Object.entries(filterOptions).forEach(([key, value]) => {
+  // Function to clear option
+  const clearOption = (filterTag: string, filterOptions: any) => {
+    Object.entries(filterOptions).forEach(([key, value]: any[]) => {
       if (value.every((val: any[]) => typeof val === 'string')) {
-        if(value.length  <= 1) {
-          const newFilterOptions: SelectedFilterOptionsInterface = {
-            ...filterOptions
-          }
-
-          const optionKey: any = findKeyByValue(filterOptions, filterTag)
-
-          delete newFilterOptions[optionKey]
-
-          setSelectedFilterOptions({
-            ...newFilterOptions
-          })
-        } else {
-          const filteredOptionArray = filterOptions[key].filter((option) => {
-            return option !== filterTag
-          })
-
-          setSelectedFilterOptions({
-            ...filterOptions,
-            [key]: filteredOptionArray
-          })
-        }
-      } else if(value.every((val: any) => typeof val === 'object')) {
-        value.forEach((val: any) => {
-
-          const newSubArray = value.filter((option) => option !== val)
-
-          Object.entries(val).forEach(([subKey, subValue]: any[]) => {
-            console.log()
-            if(subValue.length <= 1) {
-
-              if(newSubArray.length <= 1) {
-                const newFilterOptions = {
-                  ...filterOptions
-                }
-
-                delete newFilterOptions[key]
-
-                setSelectedFilterOptions({
-                  ...newFilterOptions
-                })
-              }
-            } else {
-              const newSubArray = subValue.filter((option: string) => option !== filterTag)
-              const subValueIndex = value.findIndex(({key, array}) => array === subValue )
-              value[subValueIndex] = newSubArray
-
-              const newFilterOptions = {
-                ...filterOptions
-              }
-
-              const objectIndex = newFilterOptions[key].findIndex((option) => {
-                return option === val
-              })
-
-              newFilterOptions[key][objectIndex][subKey] = newSubArray
-
-              console.log('NEW FILTER OPTIONS',newFilterOptions)
-
-              setSelectedFilterOptions(
-                newFilterOptions
-              )
-            }
-          })
-        })
+        handleStringOptions(key, value, filterTag, filterOptions);
+      } else if (value.every((val: any) => typeof val === 'object')) {
+        handleObjectOptions(key, value, filterTag, filterOptions);
       }
+    });
+  };
+
+  // Function to handle clearing string options
+  const handleStringOptions = (key: string, value: any[], filterTag: string, filterOptions: any) => {
+    if (value.length <= 1) {
+      const newFilterOptions = { ...filterOptions };
+      const optionKey: any = findKeyByValue(filterOptions, filterTag);
+      delete newFilterOptions[optionKey];
+      setSelectedFilterOptions({ ...newFilterOptions });
+    } else {
+      const filteredOptionArray = value.filter((option: any) => option !== filterTag);
+      setSelectedFilterOptions({ ...filterOptions, [key]: filteredOptionArray });
+    }
+  };
+
+  // Function to handle clearing object options
+  const handleObjectOptions = (key: string, value: any[], filterTag: string, filterOptions: any) => {
+    value.forEach((val: any) => {
+      const newSubArray = value.filter((option: any) => option !== val);
+      Object.entries(val).forEach(([subKey, subValue]: any[]) => {
+        if (subValue.length <= 1 && newSubArray.length <= 1) {
+          const newFilterOptions = { ...filterOptions };
+          delete newFilterOptions[key];
+          setSelectedFilterOptions({ ...newFilterOptions });
+        } else {
+          const newSubArray = subValue.filter((option: string) => option !== filterTag);
+          const subValueIndex = value.findIndex(({ unused_, array }: any) => array === subValue);
+          value[subValueIndex] = newSubArray;
+          const newFilterOptions = { ...filterOptions };
+          const objectIndex = newFilterOptions[key].findIndex((option: any) => option === val);
+          newFilterOptions[key][objectIndex][subKey] = newSubArray;
+          setSelectedFilterOptions(newFilterOptions);
+        }
+      });
     });
   };
 
   return (
     <div key={tagKey} className="filter-tag-container">
-      <p className=' ion-no-padding ion-no-margin'>{filterOption.toUpperCase()}</p>
-      <IonButton onClick={() => clearOption(filterOption, (selectedFilterOptions as SelectedFilterOptionsInterface))} fill='clear' color='danger' className='ion-no-padding'>
+      <p className='ion-no-padding ion-no-margin'>{filterOption.toUpperCase()}</p>
+      <IonButton onClick={() => clearOption(filterOption, selectedFilterOptions)} fill='clear' color='danger' className='ion-no-padding'>
         <IonIcon icon={close} />
       </IonButton>
     </div>
