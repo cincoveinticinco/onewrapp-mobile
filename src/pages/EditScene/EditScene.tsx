e43ab5ccb7bfdca93600b6e81030c0c958408d4d
Daniel <carreraprogrammer@gmail.com>
2024-02-19 14:07:35 -0500

    Refactor EditScene component and add form functionality

--- src/pages/EditScene/EditScene.tsx
+++ src/pages/EditScene/EditScene.tsx
@@ -1,12 +1,94 @@
 import { IonContent } from '@ionic/react';
-import { useRef } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import AddScenesForm from '../../components/AddScene/AddSceneForm';
 import useHideTabs from '../../hooks/useHideTabs';
 import SecondaryPagesLayout from '../../Layouts/SecondaryPagesLayout/SecondaryPagesLayout';
 import { useHistory, useParams } from 'react-router';
+import useHandleBack from '../../hooks/useHandleBack';
+import { useForm } from 'react-hook-form';
+import DatabaseContext from '../../context/database';
+import useSuccessToast from '../../hooks/useSuccessToast';
+import useErrorToast from '../../hooks/useErrorToast';
 
 const EditScene: React.FC = () => {
+  const handleBack = useHandleBack()
   const contentRef = useRef<HTMLIonContentElement>(null);
+  const { id } = useParams<{ id: string }>();
+  const projectId = parseInt(id);
+  const updatedAt = new Date().toISOString();
+  const { oneWrapDb } = useContext<any>(DatabaseContext);
+  const successMessageToast = useSuccessToast()
+  const errorToast = useErrorToast()
+
+  const sceneDefaultValues = {
+    projectId,
+    id: null,
+    episodeNumber: null,
+    sceneNumber: null,
+    sceneType: null,
+    protectionType: null,
+    intOrExtOption: null,
+    dayOrNightOption: null,
+    locationName: null,
+    setName: null,
+    scriptDay: null,
+    year: null,
+    synopsis: null,
+    page: null,
+    pages: null,
+    estimatedSeconds: null,
+    characters: null,
+    extras: null,
+    elements: null,
+    notes: [],
+    updatedAt,
+  };
+
+
+  const [formData, setFormData] = useState<any>(sceneDefaultValues);
+  const { sceneId }: any = useParams();
+
+  const getExistingScene = async () => {
+    const scene = await oneWrapDb.scenes.findOne({ selector: { id: sceneId } }).exec();
+    console.log('Existing scene:', scene._data);
+    return scene._data;
+  };
+
+  useEffect(() => {
+    const fetchScene = async () => {
+      if (sceneId) {
+        const existingScene = await getExistingScene();
+        console.log(existingScene)
+        Object.keys(existingScene).forEach((key) => {
+          setValue(key, existingScene[key]);
+          setFormData({ ...formData, [key]: existingScene[key] });
+        });
+      }
+    };
+
+    fetchScene();
+  }, [sceneId]);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+    watch,
+    getValues
+  } = useForm({ defaultValues: formData });
+
+
+  const currentValues = getValues();
+
+  useEffect(() => {
+    console.log('Current values:', currentValues);
+  }, [currentValues])
+
+  const handleSetValue = (fieldName: string, value: any) => {
+    setValue(fieldName, value);
+  };
 
   useHideTabs();
 
@@ -14,16 +96,51 @@ const EditScene: React.FC = () => {
     contentRef.current?.scrollToTop();
   };
 
-  const { id } = useParams<{ id: string }>();
+  const sceneFormId = 'edit-scene-form-id'
 
-  const history = useHistory();
+  const updateScene = async (formData: any) => {
+    try {
+      console.log('Updating scene:', formData);
 
-  const handleConfirm = () => history.push(`/my/projects/${id}/strips`);
+      await oneWrapDb?.scenes.upsert(formData);
+
+      successMessageToast("Scene updated successfully!");
+      handleBack()
+    } catch (error: any) {
+      console.log('Error updating scene:', error);
+      errorToast(error ? error.message : 'Error updating scene');
+      scrollToTop();
+    }
+
+    scrollToTop();
+  };
+
+  const onSubmit = (formData: any): void => {
+    scrollToTop();
+    sceneId ? updateScene(formData) : console.error('No sceneId found');
+  }
+
+  const handleConfirm = () => {
+    scrollToTop();
+    handleSubmit(onSubmit)();
+  }
 
   return (
-    <SecondaryPagesLayout resetSelections={handleConfirm} pageTitle="Edit Scene" handleConfirm={handleConfirm}>
+    <SecondaryPagesLayout resetSelections={handleBack} pageTitle="Edit Scene" handleConfirm={handleConfirm}>
       <IonContent color="tertiary" ref={contentRef}>
-        <AddScenesForm scrollToTop={() => scrollToTop()} editMode= {true}/>
+        <AddScenesForm 
+          scrollToTop={() => scrollToTop()} 
+          editMode= {true} 
+          sceneFormId={sceneFormId} 
+          handleSubmit={handleSubmit}
+          control={control}
+          errors={errors}
+          reset={reset}
+          setValue={setValue}
+          watch={watch}
+          formData={formData}
+          onSubmit={onSubmit}
+        />
       </IonContent>
     </SecondaryPagesLayout>
   );
