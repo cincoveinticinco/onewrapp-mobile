d04f7ae486899e4edcd2c3cac5dc63fb543e73fd
Daniel <carreraprogrammer@gmail.com>
2024-02-19 14:04:15 -0500

    Add useErrorToast hook

---
+++ src/hooks/useErrorToast.tsx
@@ -0,0 +1,21 @@
+// useErrorToast.js
+import { useIonToast } from '@ionic/react';
+import { closeCircle } from 'ionicons/icons';
+
+const useErrorToast = () => {
+  const [presentToast] = useIonToast();
+
+  const errorMessageToast = (message: string) => {
+    presentToast({
+      message: message,
+      duration: 2000,
+      position: 'top',
+      icon: closeCircle,
+      cssClass: 'error-toast',
+    });
+  };
+
+  return errorMessageToast;
+};
+
+export default useErrorToast;
