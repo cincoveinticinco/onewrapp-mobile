c47af5b1a0584593f315c51c43b3251f61fe38e2
Daniel <carreraprogrammer@gmail.com>
2024-02-19 14:08:10 -0500

    Refactor SecondaryPagesLayout component

--- src/Layouts/SecondaryPagesLayout/SecondaryPagesLayout.tsx
+++ src/Layouts/SecondaryPagesLayout/SecondaryPagesLayout.tsx
@@ -1,16 +1,14 @@
 import { IonHeader, IonPage } from '@ionic/react';
 import React from 'react';
 import ModalToolbar from '../../components/Shared/ModalToolbar/ModalToolbar';
-import useHandleBack from '../../hooks/useHandleBack';
 import capitalizeString from '../../utils/capitalizeString';
-import { useParams } from 'react-router';
 
 interface SecondaryPagesLayoutProps {
   children: React.ReactNode
   resetSelections?: () => void
   saveOptions?: () => void
   pageTitle: string
-  handleConfirm: () => void
+  handleConfirm: any
 }
 
 const SecondaryPagesLayout: React.FC<SecondaryPagesLayoutProps> = ({
