ed35ce04d576ea791a8d8928c1e26b8e7a03d969
Daniel <carreraprogrammer@gmail.com>
2024-02-20 09:53:04 -0500

    Refactor Sets page to display set statistics

--- src/pages/Sets/Sets.tsx
+++ src/pages/Sets/Sets.tsx
@@ -1,19 +1,77 @@
+import React, { useContext, useEffect, useState } from 'react';
 import {
-  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
 } from '@ionic/react';
-import ExploreContainer from '../../components/Shared/ExploreContainer/ExploreContainer';
+import DatabaseContext from '../../context/database';
+import getUniqueValuesByKey from '../../utils/getUniqueValuesByKey';
+import { SceneTypeEnum } from '../../Ennums/ennums';
 
-const Sets: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar color="tertiary">
-        <IonTitle>SETS</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent color="tertiary" fullscreen>
-      <ExploreContainer name="Sets page" />
-    </IonContent>
-  </IonPage>
-);
+const Sets: React.FC = () => {
+  const { offlineScenes } = useContext(DatabaseContext);
+  const [sets, setSets] = useState<any[]>([]);
+
+  useEffect(() => {
+    const processSet = (setName: string) => {
+      const setScenes = offlineScenes.filter((scene: any) => scene._data.setName === setName);
+
+      const charactersLength = setScenes.reduce((acc: number, scene: any) => acc + scene._data.characters.length, 0);
+      const scenesQuantity = setScenes.length;
+      const protectionQuantity = setScenes.filter((scene: any) => scene._data.sceneType === SceneTypeEnum.PROTECTION).length;
+      const pagesSum = setScenes.reduce((acc: number, scene: any) => acc + (scene._data.pages || 0), 0);
+      const estimatedTimeSum = setScenes.reduce((acc: number, scene: any) => acc + (scene._data.estimatedSeconds || 0), 0);
+      const episodesQuantity = getUniqueValuesByKey(setScenes, 'episodeNumber').length;
+
+      return {
+        setName,
+        charactersLength,
+        scenesQuantity,
+        protectionQuantity,
+        pagesSum,
+        estimatedTimeSum,
+        episodesQuantity,
+      };
+    };
+
+    const uniqueSetNames: any[] = getUniqueValuesByKey(offlineScenes, 'setName');
+    const sets: any[] = uniqueSetNames.map(processSet);
+    setSets(sets.sort((a, b) => a.setName.localeCompare(b.setName)));
+  }, [offlineScenes]);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonTitle>SETS</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent color="tertiary" fullscreen>
+        {sets.map((set, index) => (
+          <IonCard key={index}>
+            <IonCardHeader>
+              <IonCardSubtitle>{set.setName}</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Characters Length: {set.charactersLength}</p>
+              <p>Scenes Quantity: {set.scenesQuantity}</p>
+              <p>Protection Quantity: {set.protectionQuantity}</p>
+              <p>Pages Sum: {set.pagesSum}</p>
+              <p>Estimated Time Sum: {set.estimatedTimeSum}</p>
+              <p>Episodes Quantity: {set.episodesQuantity}</p>
+            </IonCardContent>
+          </IonCard>
+        ))}
+      </IonContent>
+    </IonPage>
+  );
+}
 
 export default Sets;
