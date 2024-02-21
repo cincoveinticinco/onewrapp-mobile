422821e82e3cc5d34b036e4260e9ad63fa004716
Daniel <carreraprogrammer@gmail.com>
2024-02-21 13:42:13 -0500

    Refactor Strips component to remove infinite scroll functionality

--- src/pages/Strips/Strips.tsx
+++ src/pages/Strips/Strips.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, Suspense, useContext, useRef, useMemo, useCallback } from 'react';
-import { IonButton, IonContent, IonGrid, IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/react';
+import { IonButton, IonContent, IonGrid } from '@ionic/react';
 import './Strips.scss';
 import { useHistory, useLocation } from 'react-router';
 import ScenesContext from '../../context/ScenesContext';
@@ -10,17 +10,14 @@ import DatabaseContext from '../../context/database';
 import SceneCard from '../../components/Strips/SceneCard';
 import { Scene } from '../../interfaces/scenesTypes';
 import StripTagsToolbar from '../../components/Strips/StripTagsToolbar';
-
-const BATCH_SIZE = 15;
+import ScrollInfiniteContext from '../../context/ScrollInfiniteContext';
 
 const Strips: React.FC = () => {
   const { offlineScenes } = useContext(DatabaseContext);
   const [filteredScenes, setFilteredScenes] = useState<Scene[]>([]);
   const [displayedScenes, setDisplayedScenes] = useState<Scene[]>([]);
-  const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
-  const [currentBatch, setCurrentBatch] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [error, unused_] = useState(false);
   const { selectedFilterOptions, setSelectedFilterOptions, selectedSortOptions } = useContext<any>(ScenesContext);
   const thisPath = useLocation();
   const contentRef = useRef<HTMLIonContentElement>(null);
@@ -44,30 +41,12 @@ const Strips: React.FC = () => {
 
   useEffect(() => {
     setFilteredScenes(newFilteredScenes);
-    setCurrentBatch(1);
-    setDisplayedScenes(newFilteredScenes.slice(0, BATCH_SIZE));
-    setInfiniteDisabled(false);
     offlineScenes.length > 0 ? setLoading(false) : setLoading(true);
   }, [selectedFilterOptions, selectedSortOptions, newFilteredScenes ]);
 
   const resetFilters = () => {
     setSelectedFilterOptions({});
   }
-
-  const loadMoreScenes = () => {
-    if (currentBatch * BATCH_SIZE >= filteredScenes.length) {
-      setInfiniteDisabled(true);
-      return;
-    }
-    const newScenes = filteredScenes.slice(0, (currentBatch + 1) * BATCH_SIZE);
-    setDisplayedScenes(newScenes);
-    setCurrentBatch(currentBatch + 1);
-  };
-
-  const handleInfinite = (e: CustomEvent<void>) => {
-    loadMoreScenes();
-    (e.target as HTMLIonInfiniteScrollElement).complete();
-  };
 
   useEffect(() => {
     contentRef.current?.scrollToTop();
@@ -124,39 +103,38 @@ const Strips: React.FC = () => {
     >
       <StripTagsToolbar />
       <IonContent scrollEvents color="tertiary" ref={contentRef} id="strips-container-ref">
-        <Suspense fallback={<div>Loading scene...</div>}>
-          {loading ? (
-            <div>Loading scenes...</div>
-          ) : error ? (
-            <div>Error loading scenes. Please try again later.</div>
-          ) : (
-            <>
-              {filteredScenes.length === 0 && Object.keys(selectedFilterOptions).length > 0 ? (
-                <div className="no-items-message">
-                  <p className="ion-no-margin">There are not any scenes that match your search. </p>
-                  <IonButton
-                    fill="clear"
-                    color="primary"
-                    className="ion-no-margin reset-filters-option"
-                    onClick={resetFilters}
-                  >
-                    Reset Filters
-                  </IonButton>
-                  <span>?</span>
-                </div>
-              ) : (
-                <IonGrid className="scenes-grid ion-margin">
-                  {displayedScenes.map((scene: any, i: any) => (
-                    <SceneCard key={`scene-item-${scene}-${i}`} scene={scene} searchText={searchText} />
-                  ))}
-                  <IonInfiniteScroll onIonInfinite={handleInfinite} threshold="100px" disabled={isInfiniteDisabled}>
-                    <IonInfiniteScrollContent loadingSpinner="bubbles" loadingText="Loading more scenes..." />
-                  </IonInfiniteScroll>
-                </IonGrid>
-              )}
-            </>
-          )}
-        </Suspense>
+          <Suspense fallback={<div>Loading scene...</div>}>
+            {loading ? (
+              <div>Loading scenes...</div>
+            ) : error ? (
+              <div>Error loading scenes. Please try again later.</div>
+            ) : (
+              <>
+                {filteredScenes.length === 0 && Object.keys(selectedFilterOptions).length > 0 ? (
+                  <div className="no-items-message">
+                    <p className="ion-no-margin">There are not any scenes that match your search. </p>
+                    <IonButton
+                      fill="clear"
+                      color="primary"
+                      className="ion-no-margin reset-filters-option"
+                      onClick={resetFilters}
+                    >
+                      Reset Filters
+                    </IonButton>
+                    <span>?</span>
+                  </div>
+                ) : (
+                  <IonGrid className="scenes-grid ion-margin">
+                    <ScrollInfiniteContext setDisplayedData={setDisplayedScenes} filteredData={filteredScenes}>
+                      {displayedScenes.map((scene: any, i: any) => (
+                        <SceneCard key={`scene-item-${scene}-${i}`} scene={scene} searchText={searchText} />
+                      ))}
+                    </ScrollInfiniteContext>
+                  </IonGrid>
+                )}
+              </>
+            )}
+          </Suspense>
       </IonContent>
     </MainPagesLayout>
   );
