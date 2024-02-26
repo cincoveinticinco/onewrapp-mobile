120c26f5151e7fcc945ef34a23f76921bb132274
Daniel <carreraprogrammer@gmail.com>
2024-02-26 16:44:03 -0500

    Update user authentication logic to include create option

--- src/components/Shared/InputModal/InputModal.tsx
+++ src/components/Shared/InputModal/InputModal.tsx
@@ -1,5 +1,5 @@
 import {
-  IonCheckbox, IonContent, IonHeader, IonItem, IonList, IonModal,
+  IonCheckbox, IonContent, IonHeader, IonInput, IonItem, IonList, IonModal,
 } from '@ionic/react';
 import React, { useRef, useState } from 'react';
 import useIsMobile from '../../../hooks/useIsMobile';
@@ -11,6 +11,16 @@ import removeNumberAndDot from '../../../utils/removeNumberAndDot';
 import ModalToolbar from '../ModalToolbar/ModalToolbar';
 import truncateString from '../../../utils/truncateString';
 import HighlightedText from '../HighlightedText/HighlightedText';
+import InputItem from '../../AddScene/AddSceneFormInputs/InputItem';
+import { useForm } from 'react-hook-form';
+
+interface FormInputsProps {
+  label: string;
+  placeholder: string;
+  fieldName: string;
+  inputName: string;
+  type: string;
+}
 
 interface InputModalProps {
   optionName: string;
@@ -18,10 +28,15 @@ interface InputModalProps {
   modalTrigger: string;
   handleCheckboxToggle: (option: string) => void;
   selectedOptions: string[];
+  setSelectedOptions?: (options: string[]) => void;
   clearSelections: () => void;
   multipleSelections?: boolean;
   canCreateNew?: boolean;
   editMode?: boolean;
+  createNewTrigger?: string;
+  optionCategory?: string;
+  formInputs?: FormInputsProps[];
+  existentOptions?: any[];
 }
 
 const InputModal: React.FC<InputModalProps> = ({
@@ -30,12 +45,16 @@ const InputModal: React.FC<InputModalProps> = ({
   modalTrigger,
   handleCheckboxToggle,
   selectedOptions,
+  setSelectedOptions,
   clearSelections,
   multipleSelections = true,
   canCreateNew = false,
-  editMode = false,
+  optionCategory,
+  formInputs,
+  existentOptions
 }) => {
   const [searchText, setSearchText] = useState('');
+  const [createNewMode, setCreateNewMode] = useState(false)
 
   const modalRef = useRef<HTMLIonModalElement>(null);
 
@@ -49,6 +68,7 @@ const InputModal: React.FC<InputModalProps> = ({
     if (modalRef.current) {
       modalRef.current.dismiss();
     }
+    setSearchText('');
   };
 
   const getListStyles = () => {
@@ -67,15 +87,89 @@ const InputModal: React.FC<InputModalProps> = ({
     return {};
   };
 
-  const uncheckedOptions = listOfOptions.filter((option: string) => !selectedOptions.includes(removeNumberAndDot(option)));
+  let uncheckedOptions = listOfOptions.filter((option: string) => !selectedOptions.includes(removeNumberAndDot(option)));
 
-  const filteredOptions = listOfOptions.filter((option: string) => option.toLowerCase().includes(searchText.toLowerCase()));
+  let filteredOptions = listOfOptions.filter((option: string) => option.toLowerCase().includes(searchText.toLowerCase()));
 
-  const uncheckedFilteredOptions = uncheckedOptions.filter((option: string) => option.toLowerCase().includes(searchText.toLowerCase()));
+  let uncheckedFilteredOptions = uncheckedOptions.filter((option: string) => option.toLowerCase().includes(searchText.toLowerCase()));
 
-  const checkedSelectedOptions: any[] = listOfOptions.filter((option: string) => selectedOptions.includes(removeNumberAndDot(option)));
+  let checkedSelectedOptions: any[] = listOfOptions.filter((option: string) => selectedOptions.includes(removeNumberAndDot(option)));
 
-  const isOptionChecked = (option: string) => selectedOptions.includes(removeNumberAndDot(option));
+  let isOptionChecked = (option: string) => selectedOptions.includes(removeNumberAndDot(option));
+
+  let defaultFormValues: any = {}
+
+  formInputs?.forEach((input: any) => {
+    defaultFormValues[input.fieldName] = null
+  })
+
+  const [errorMessage, setErrorMessage] = useState('REQUIRED *')
+  const [showError, setShowError] = useState(false)
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    resetField
+  } = useForm({
+    defaultValues: defaultFormValues
+  })
+
+  const handleSaveNewOption = (newOptionArgument: any) => {
+    newOptionArgument.categoryName = optionCategory
+    setSelectedOptions ? setSelectedOptions([...selectedOptions, newOptionArgument]) : ''
+    setCreateNewMode(false)
+    setSearchText('')
+    formInputs?.forEach((input: any) => {
+      resetField(input.fieldName)
+    })
+  }
+
+  const setNewOptionValue = (fieldName: string, value: string) => {
+    if((value === '' || !value) && fieldName !== 'characterNum') {
+      return setValue(fieldName, null)
+    } else {
+      return setValue(fieldName, value)
+    }
+  }
+
+  const handleValidation = (value: string, fieldName: string) => {
+    if(fieldName === 'characterNum') {
+      return true
+    }
+
+    if((value === '' || !value) && fieldName !== 'characterNum') {
+      setShowError(true)
+      setErrorMessage('REQUIRED *')
+      return 'This field is required'
+    }
+
+    const optionExists = existentOptions?.findIndex((option: any) => {
+      if(option[fieldName]) {
+        return option[fieldName].toLowerCase() === value.toLowerCase()
+      }
+    })
+
+    if (fieldName !== 'characterNum' && optionExists && optionExists > -1) {
+      console.log('SETTING TO ALREADY EXISTS')
+      setShowError(true)
+      setErrorMessage('ALREADY EXISTS *')
+      return 'This option already exists'
+    }
+
+    return true
+  }
+
+  const cancelForm = () => {
+    setCreateNewMode(false);
+    formInputs?.forEach((input: any) => {
+      resetField(input.fieldName)
+    })
+    setSearchText('')
+  }
+
+  console.log('SELECTED OPTIONS', optionName,selectedOptions)
 
   return (
     <IonModal
@@ -89,79 +183,115 @@ const InputModal: React.FC<InputModalProps> = ({
           toolbarTitle={optionName}
           handleReset={clearSelections}
           handleBack={closeModal}
-          showReset={selectedOptions.length > 0}
+          showReset={selectedOptions.length > 0 && selectedOptions[0] !== null}
         />
       </IonHeader>
-      <IonContent color="tertiary">
-        <ModalSearchBar searchText={searchText} setSearchText={setSearchText} showSearchBar={listOfOptions.length > 10} />
-        {
-           filteredOptions.length === 0
-           && (
-           <p className="no-items-message">
-             There are no coincidences. Do you want to
-             <span onClick={() => setSearchText('')} style={{ color: 'var(--ion-color-primary)' }}>reset </span>
-             ?
-           </p>
-           )
-        }
-        <>
-          <IonList color="tertiary" className="ion-no-padding ion-margin options-list" style={getListStyles()}>
-            {checkedSelectedOptions.map((option: string, i: number) => (
-              <div
-                color="tertiary"
-                key={`filter-item-${i}`}
-                className="checkbox-item-option filter-item ion-no-margin ion-no-padding"
-                onClick={() => handleCheckboxToggle(option)}
-              >
-                <IonCheckbox
-                  slot="start"
-                  className="ion-no-margin ion-no-padding checkbox-option"
-                  labelPlacement="end"
-                  checked={isOptionChecked(option)}
-                >
-                  <HighlightedText text={truncateString(option.toUpperCase(), (isMobile ? 30 : 140))} searchTerm={searchText} />
-                </IonCheckbox>
-              </div>
-            ))}
-            {uncheckedFilteredOptions.map((option: string, i: number) => (
-              <div
-                color="tertiary"
-                key={`filter-item-${i}`}
-                className="checkbox-item-option filter-item ion-no-margin ion-no-padding"
-                onClick={() => handleCheckboxToggle(option)}
-              >
-                <IonCheckbox
-                  slot="start"
-                  className="ion-no-margin ion-no-padding checkbox-option"
-                  labelPlacement="end"
-                  checked={isOptionChecked(option)}
-                  disabled={!multipleSelections && checkedSelectedOptions.length > 0}
-                >
-                  <HighlightedText text={truncateString(option.toUpperCase(), 30)} searchTerm={searchText} />
-                </IonCheckbox>
-              </div>
-            ))}
-          </IonList>
-          {
-              filteredOptions.length === 0 && canCreateNew
-                && (
-                <p className="no-items-message">
-                  There are no coincidences. Do you want to create a new one ?
-                  <span className="no-items-buttons-container ion-flex ion-justify-content-center ion-align-items-center">
-                    <OutlinePrimaryButton buttonName="CREATE NEW" className="ion-margin no-items-confirm" onClick={() => {}} />
-                    <OutlineLightButton buttonName="CANCEL" className="ion-margin cancel-button no-items-cancel" onClick={clearSearchTextModal} />
-                  </span>
-                </p>
-                )
+      {canCreateNew && createNewMode ? (
+        <IonContent color="tertiary">
+            {
+              formInputs &&
+              formInputs.map((input: any, i: any) => (
+                <InputItem
+                  key={i}
+                  label={input.label}
+                  placeholder={input.placeholder}
+                  control={control}
+                  fieldName={input.fieldName}
+                  inputName={input.inputName}
+                  displayError={showError && input.fieldName !== 'characterNum'}
+                  setValue={setNewOptionValue}
+                  validate={(value: string) => handleValidation(value, input.fieldName)}
+                  type={input.type}
+                  errorMessage={errorMessage}
+                />
+              ))
             }
-          <OutlinePrimaryButton
-            buttonName="SAVE"
-            onClick={closeModal}
-            className="ion-margin modal-confirm-button"
-          />
-          {isMobile && <OutlineLightButton buttonName="CANCEL" onClick={closeModal} className="ion-margin cancel-input-modal-button cancel-button" />}
-        </>
-      </IonContent>
+            <div className='add-new-option-buttons-container'>       
+              <OutlinePrimaryButton
+                buttonName="SAVE"
+                onClick={handleSubmit(handleSaveNewOption)}
+                className="ion-margin modal-confirm-button"
+              />
+
+              <OutlineLightButton 
+                buttonName="CANCEL" 
+                onClick={cancelForm}
+                className="ion-margin cancel-input-modal-button cancel-button" 
+              />
+            </div>
+        </IonContent>
+      ) : (
+        <IonContent color="tertiary">
+          <ModalSearchBar searchText={searchText} setSearchText={setSearchText} showSearchBar={listOfOptions.length > 10} />
+          {
+            filteredOptions.length === 0
+            && (
+            <p className="no-items-message">
+              There are no coincidences. Do you want to
+              <span onClick={() => setSearchText('')} style={{ color: 'var(--ion-color-primary)' }}>reset </span>
+              ?
+            </p>
+            )
+          }
+          <>
+            <IonList color="tertiary" className="ion-no-padding ion-margin options-list" style={getListStyles()}>
+              {checkedSelectedOptions.map((option: string, i: number) => (
+                <div
+                  color="tertiary"
+                  key={`filter-item-${i}`}
+                  className="checkbox-item-option filter-item ion-no-margin ion-no-padding"
+                  onClick={() => handleCheckboxToggle(option)}
+                >
+                  <IonCheckbox
+                    slot="start"
+                    className="ion-no-margin ion-no-padding checkbox-option"
+                    labelPlacement="end"
+                    checked={isOptionChecked(option)}
+                  >
+                    <HighlightedText text={truncateString(option.toUpperCase(), (isMobile ? 30 : 140))} searchTerm={searchText} />
+                  </IonCheckbox>
+                </div>
+              ))}
+              {uncheckedFilteredOptions.map((option: string, i: number) => (
+                <div
+                  color="tertiary"
+                  key={`filter-item-${i}`}
+                  className="checkbox-item-option filter-item ion-no-margin ion-no-padding"
+                  onClick={() => handleCheckboxToggle(option)}
+                >
+                  <IonCheckbox
+                    slot="start"
+                    className="ion-no-margin ion-no-padding checkbox-option"
+                    labelPlacement="end"
+                    checked={isOptionChecked(option)}
+                    disabled={!multipleSelections && checkedSelectedOptions.length > 0}
+                  >
+                    <HighlightedText text={truncateString(option.toUpperCase(), 30)} searchTerm={searchText} />
+                  </IonCheckbox>
+                </div>
+              ))}
+            </IonList>
+            {
+                filteredOptions.length === 0 && canCreateNew
+                  && (
+                  <p className="no-items-message">
+                    There are no coincidences. Do you want to create a new one ?
+                    <span className="no-items-buttons-container ion-flex ion-justify-content-center ion-align-items-center">
+                      <OutlinePrimaryButton buttonName="CREATE NEW" className="ion-margin no-items-confirm" onClick={() => setCreateNewMode(true)} />
+                      <OutlineLightButton buttonName="CANCEL" className="ion-margin cancel-button no-items-cancel" onClick={clearSearchTextModal} />
+                    </span>
+                  </p>
+                  )
+              }
+            <OutlinePrimaryButton
+              buttonName="SAVE"
+              onClick={closeModal}
+              className="ion-margin modal-confirm-button"
+            />
+            {isMobile && <OutlineLightButton buttonName="CANCEL" onClick={closeModal} className="ion-margin cancel-input-modal-button cancel-button" />}
+          </>
+        </IonContent>
+      )}
     </IonModal>
   );
 };
