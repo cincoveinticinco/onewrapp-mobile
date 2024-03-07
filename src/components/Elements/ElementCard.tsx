d737ea03bf4048182f4f95c21d0adc52b55c1af3
Daniel <carreraprogrammer@gmail.com>
2024-03-07 10:48:46 -0500

    Implement edit and delete functionalities for elements and categories

--- src/components/Elements/ElementCard.tsx
+++ src/components/Elements/ElementCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   IonItemSliding,
   IonItemOptions,
@@ -6,15 +6,20 @@ import {
   IonIcon,
   IonItem,
   IonTitle,
+  useIonToast,
 } from '@ionic/react';
 import HighlightedText from '../../components/Shared/HighlightedText/HighlightedText';
 import './ElementCard.scss';
 import secondsToMinSec from '../../utils/secondsToMinSec';
 import floatToFraction from '../../utils/floatToFraction';
 import { FiTrash } from 'react-icons/fi';
-import { banOutline, pencilOutline } from 'ionicons/icons';
+import { banOutline, checkmarkCircle, pencilOutline } from 'ionicons/icons';
 import DropDownButton from '../Shared/DropDownButton/DropDownButton';
 import useIsMobile from '../../hooks/useIsMobile';
+import EditionModal from '../Shared/EditionModal/EditionModal';
+import DatabaseContext from '../../context/database';
+import useErrorToast from '../../hooks/useErrorToast';
+import InputAlert from '../Shared/InputAlert/InputAlert';
 
 interface Element {
   elementName: string;
@@ -27,6 +32,7 @@ interface Element {
   participation: string;
   category: string;
   categoryName: string;
+  elementCategory?: string;
 }
 
 interface ElementCardProps {
@@ -50,6 +56,21 @@ const InfoLabel: React.FC<{ label: string, value: string | number, symbol?: stri
 
 const ElementCard: React.FC<ElementCardProps> = ({ data, searchText, section, isOpen = false, onClick, elementsQuantity }) => {
   const isMobile = useIsMobile();
+  const { oneWrapDb } = useContext<any>(DatabaseContext);
+
+  const [presentToast] = useIonToast();
+
+  const successMessageSceneToast = (message: string) => {
+    presentToast({
+      message,
+      duration: 2000,
+      icon: checkmarkCircle,
+      position: 'top',
+      cssClass: 'success-toast',
+    });
+  };
+
+  const errorToast = useErrorToast();
   
   const divideIntegerFromFraction = (value: string) => {
     const [integer, fraction] = value.split(' ');
@@ -84,6 +105,167 @@ const ElementCard: React.FC<ElementCardProps> = ({ data, searchText, section, is
       }
 
       return 'N/A'
+    }
+  }
+
+  const scenesToEditWithElement = () => oneWrapDb.scenes.find({
+    selector: {
+      'elements.elementName': data.elementName,
+    }
+  }).exec();
+
+  const scenesToEditWithCategory = () => oneWrapDb.scenes.find({
+    selector: {
+      'elements.categoryName': data.categoryName,
+    }
+  }).exec()
+
+  const formElementInputs = [
+    {
+      label: 'Element Name',
+      type: 'text',
+      fieldName: 'elementName',
+      placeholder: 'INSERT',
+      required: true,
+      inputName: 'add-element-name-input',
+    },
+  ]
+
+  const formCategoryInputs = [
+    {
+      label: 'Category Name',
+      type: 'text',
+      fieldName: 'categoryName',
+      placeholder: 'INSERT',
+      required: true,
+      inputName: 'add-category-name-input',
+    },
+  ]
+
+  const defaultFormValuesForElements = {
+    elementName: data.elementName,
+  }
+
+  const defaultFormValuesForCategories = {
+    categoryName: data.categoryName,
+  }
+
+  const editElement = async (newElement: any) => {
+    try {
+      const scenes = await scenesToEditWithElement();
+      const updatedScenes: any = [];
+  
+      scenes.forEach((scene: any) => {
+        const updatedScene = { ...scene._data };
+
+        newElement.categoryName = data.elementCategory;
+  
+        updatedScene.elements = updatedScene.elements.filter((el: any) => el.elementName !== data.elementName).concat(newElement);
+        
+        updatedScenes.push(updatedScene);
+      });
+
+      const result = await oneWrapDb.scenes.bulkUpsert(updatedScenes);
+  
+      console.log('Bulk update result:', result);
+  
+      console.log('Character deleted');
+
+      successMessageSceneToast(`${data.elementName ? data.elementName.toUpperCase() : 'NO NAME'} was successfully updated!`);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const deleteElement = async () => {
+    try {
+      const scenes = await scenesToEditWithElement();
+      const updatedScenes: any = [];
+  
+      scenes.forEach((scene: any) => {
+        const updatedScene = { ...scene._data };
+  
+        updatedScene.elements = updatedScene.elements.filter((el: any) => el.elementName !== data.elementName);
+        
+        console.log('Updated Scene:', updatedScene);
+        
+        updatedScenes.push(updatedScene);
+      });
+
+      const result = await oneWrapDb.scenes.bulkUpsert(updatedScenes);
+  
+      console.log('Bulk update result:', result);
+  
+      console.log('Element deleted');
+
+      successMessageSceneToast(`${data.elementName ? data.elementName.toUpperCase() : 'NO NAME'} was successfully deleted from all scenes!`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const deleteCategory = async () => {
+    try {
+      const scenes = await scenesToEditWithCategory();
+      const updatedScenes: any = [];
+  
+      scenes.forEach((scene: any) => {
+        const updatedScene = { ...scene._data };
+  
+        updatedScene.elements = updatedScene.elements.filter((el: any) => el.categoryName !== data.categoryName);
+        
+        console.log('Updated Scene:', updatedScene);
+        
+        updatedScenes.push(updatedScene);
+      });
+
+      const result = await oneWrapDb.scenes.bulkUpsert(updatedScenes);
+  
+      console.log('Bulk update result:', result);
+  
+      console.log('Category deleted');
+
+      successMessageSceneToast(`${data.categoryName ? data.categoryName.toUpperCase() : 'N/A'} was successfully deleted from all scenes!`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const editCategory = async (newCategory: any) => {
+    try {
+      const scenes = await scenesToEditWithCategory();
+      const updatedScenes: any = [];
+  
+      scenes.forEach((scene: any) => {
+        const updatedScene = { ...scene._data };
+
+        const newElements: any[] = []
+  
+        updatedScene.elements.forEach((el: any) => {
+          const newElement = { ...el };
+          if(el.categoryName === data.categoryName) {
+            newElement.categoryName = newCategory.categoryName;
+          }
+          newElements.push(newElement);
+        })
+
+        updatedScene.elements = newElements;
+        
+        updatedScenes.push(updatedScene);
+      });
+
+      const result = await oneWrapDb.scenes.bulkUpsert(updatedScenes);
+  
+      console.log('Bulk update result:', result);
+  
+      console.log('Character deleted');
+
+      successMessageSceneToast(`${data.categoryName ? data.categoryName.toUpperCase() : 'NO NAME'} was successfully updated!`);
+
+    } catch (error: any) {
+      console.error(error);
+      errorToast(error ? error : 'Error updating category');
     }
   }
 
@@ -126,17 +308,42 @@ const ElementCard: React.FC<ElementCardProps> = ({ data, searchText, section, is
       </IonItem>
       <IonItemOptions className="element-card-item-options">
         <div className="buttons-wrapper">
-          <IonButton fill="clear">
+          <IonButton fill="clear" id={section === 'category' ? `edit-category-${data.categoryName}` : `edit-element-${data.elementName}`}>
             <IonIcon icon={pencilOutline} className="button-icon view" />
           </IonButton>
-          <IonButton fill="clear">
+          <IonButton fill="clear" onClick={() => section === 'category' ? scenesToEditWithCategory().then((values: any) => console.log(values)) : scenesToEditWithElement().then((values: any)  => console.log(values))}>
             <IonIcon icon={banOutline} className="button-icon ban" />
           </IonButton>
-          <IonButton fill="clear">
+          <IonButton fill="clear" id={section === 'category' ? `delete-category-${data.categoryName}` : `delete-element-${data.elementName}`}>
             <FiTrash className="button-icon trash" />
           </IonButton>
         </div>
       </IonItemOptions>
+
+      <EditionModal
+        formInputs={section === 'category' ? formCategoryInputs : formElementInputs}
+        handleEdition={section === 'category' ? editCategory : editElement}
+        modalTrigger={section === 'category' ? `edit-category-${data.categoryName}` : `edit-element-${data.elementName}`}
+        title='Edit Element'
+        defaultFormValues={section === 'category' ? defaultFormValuesForCategories : defaultFormValuesForElements}
+      />
+
+      <InputAlert
+        header="Delete Category"
+        message={`Are you sure you want to delete ${data.categoryName ? data.categoryName.toUpperCase() : 'N/A'} category from all the scenes? This will also delete all the elements inside this category.`}
+        handleOk={() => deleteCategory()}
+        inputs={[]}
+        trigger={`delete-category-${data.categoryName}`}
+      />
+
+      <InputAlert
+        header="Delete Element"
+        message={`Are you sure you want to delete ${data.elementName ? data.elementName.toUpperCase() : 'NO NAME'} element from all the scenes?`}
+        handleOk={() => deleteElement()}
+        inputs={[]}
+        trigger={`delete-element-${data.elementName}`}
+      />
+
     </IonItemSliding>
   );
 };
