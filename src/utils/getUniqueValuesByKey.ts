6dcd0e87ca4ffae3507b1676a7d1284d25cdf558
Daniel <carreraprogrammer@gmail.com>
2024-03-13 11:34:04 -0500

    Refactor getUniqueValuesByKey function to use keyof operator for key parameter

--- src/utils/getUniqueValuesByKey.ts
+++ src/utils/getUniqueValuesByKey.ts
@@ -4,8 +4,8 @@ interface SceneWithIndexSignature extends Scene {
   [key: string]: any;
 }
 
-function getUniqueValuesByKey(array: SceneWithIndexSignature[], key: string | number) {
-  const uniqueValues = new Set();
+function getUniqueValuesByKey(array: SceneWithIndexSignature[], key: keyof SceneWithIndexSignature) {
+  const uniqueValues = new Set<SceneWithIndexSignature[keyof SceneWithIndexSignature]>();
   array.forEach((item) => {
     if (item[key]) {
       uniqueValues.add(item[key]);
@@ -13,4 +13,16 @@ function getUniqueValuesByKey(array: SceneWithIndexSignature[], key: string | nu
   });
   return Array.from(uniqueValues);
 }
+
 export default getUniqueValuesByKey;
+
+// EXAMPLE
+// const scenes = [
+//   { id: 1, setName: 'A' },
+//   { id: 2, setName: 'B' },
+//   { id: 3, setName: 'A' },
+//   { id: 4, setName: 'C' },
+//   { id: 5, setName: 'A' },
+//   { id: 6, setName: 'B' }];
+// getUniqueValuesByKey(scenes, 'setName');
+// // ['A', 'B', 'C']
