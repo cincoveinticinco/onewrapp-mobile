259d62b648af9f6b89738834cad250d44e6cfda5
Daniel <carreraprogrammer@gmail.com>
2024-03-19 21:02:37 -0500

    Refactor HighlightedTextWithArray component to support multiple search terms

--- src/components/Shared/HighlightedTextWithArray/HighlightedTextWithArray.tsx
+++ src/components/Shared/HighlightedTextWithArray/HighlightedTextWithArray.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './HighlightedTextWithArray.scss';
 import removeAccents from '../../../utils/removeAccents';
 
+export interface SearchTerm {
+  searchTerm: string;
+  categoryName: string;
+  type: string;
+  highlightColor: string;
+}
+
 interface HighlightedTextWithArrayProps {
   text: string;
-  searchTerms: string[];
-  highlightColor?: string;
+  searchTerms: SearchTerm[];
   textColor?: string;
 }
 
-const HighlightedTextWithArray: React.FC<HighlightedTextWithArrayProps> = ({ text, searchTerms, highlightColor = 'var(--ion-color-primary)', textColor = 'var(--ion-color-dark)' }) => {
+const HighlightedTextWithArray: React.FC<HighlightedTextWithArrayProps> = ({
+  text,
+  searchTerms,
+  textColor = 'var(--ion-color-dark)'
+}) => {
   const normalizedText = removeAccents(text).toLowerCase();
-  const normalizedSearchTerms = searchTerms.map(term => removeAccents(term).toLowerCase());
+  const normalizedSearchTerms = searchTerms.map(term => removeAccents(term.searchTerm).toLowerCase());
 
   const getOriginalPart = (part: string) => {
     const partIndex = normalizedText.indexOf(part.toLowerCase());
@@ -20,13 +30,18 @@ const HighlightedTextWithArray: React.FC<HighlightedTextWithArrayProps> = ({ tex
 
   const parts = normalizedText.split(new RegExp(`(${normalizedSearchTerms.join('|')})`, 'gi'));
 
+  const getBackgroundColor = (part: string) => {
+    const searchTerm = searchTerms.find(term => removeAccents(term.searchTerm).toLowerCase() === removeAccents(part).toLowerCase());
+    return searchTerm ? searchTerm.highlightColor : '';
+  }
+
   return (
     <span className="highlighted-text">
       {parts.map((part: any, index: any) => {
         const normalizedPart = removeAccents(part).toLowerCase();
         const isHighlighted = normalizedSearchTerms.includes(normalizedPart);
         return isHighlighted ? (
-          <mark style={{ backgroundColor: highlightColor, color: textColor }} key={index}>
+          <mark style={{ backgroundColor: getBackgroundColor(part), color: textColor }} key={index}>
             {getOriginalPart(part)}
           </mark>
         ) : (
