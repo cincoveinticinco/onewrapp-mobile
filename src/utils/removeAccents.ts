99630a92620c1efcbfef368e42b7fd2a1ad0fced
Daniel <carreraprogrammer@gmail.com>
2024-03-21 12:16:32 -0500

    Refactor removeAccents function to improve readability

--- src/utils/removeAccents.ts
+++ src/utils/removeAccents.ts
@@ -1,6 +1,8 @@
 function removeAccents(text: string) {
   if (!text) return '';
-  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  return text
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
 }
 
 export default removeAccents;
