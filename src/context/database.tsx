e2314e52204748f4c98be2ff5d31ddb81843e4d6
carreraprogrammer <carreraprogrammer@gmail.com>
2024-04-18 11:03:45 -0500

    Update database replication mechanism and add useNavigatorOnLine hook

--- src/context/database.tsx
+++ src/context/database.tsx
@@ -6,6 +6,8 @@ import ProjectsSchema from '../RXdatabase/schemas/projects';
 import { Scene } from '../interfaces/scenesTypes';
 import SceneParagraphsSchema from '../RXdatabase/schemas/paragraphs';
 import HttpReplicator from '../RXdatabase/replicator';
+import useNavigatorOnLine from '../hooks/useNavigatorOnline';
+import { useLocation } from 'react-router';
 
 const sceneCollection = new ScenesSchema();
 const paragraphCollection = new SceneParagraphsSchema();
@@ -17,13 +19,24 @@ const RXdatabase = new AppDataBase([sceneCollection, projectCollection, paragrap
 const DatabaseContext = React.createContext<any>({
   oneWrapDb: null,
   offlineScenes: [],
+  setStartReplication: () => { },
 });
 
 export const DatabaseContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [oneWrapRXdatabase, setOneWrapRXdatabase] = useState<any | null>(null);
   const [offlineScenes, setOfflineScenes] = useState<any[]>([]);
+  const [startReplication, setStartReplication] = useState(false);
   const scenesQuery = oneWrapRXdatabase?.scenes.find();
   const offlineScenes$: Observable<Scene[]> = scenesQuery ? scenesQuery.$ : null;
+  const isOnline = useNavigatorOnLine();
+
+  useEffect(() => {
+    if (!isOnline) {
+      console.log('You are offline');
+    } else {
+      console.log('You are online');
+    }
+  }, [isOnline]);
 
   useEffect(() => {
     const initializeDatabase = async () => {
@@ -37,7 +50,7 @@ export const DatabaseContextProvider = ({ children }: { children: React.ReactNod
       }
     };
 
-    if (!oneWrapRXdatabase) {
+    if (!oneWrapRXdatabase && isOnline) {
       initializeDatabase();
     }
   }, []);
@@ -53,20 +66,22 @@ export const DatabaseContextProvider = ({ children }: { children: React.ReactNod
   }, [offlineScenes$]);
 
   useEffect(() => {
-   
-      const initializeReplication = () => {
-        if (oneWrapRXdatabase) {
-          const replicator2 = new HttpReplicator(oneWrapRXdatabase, [sceneCollection], projectId);
-          replicator2.startReplicationPush();
-        }
-      };
+    const initializeReplication = () => {
+      const replicator = new HttpReplicator(oneWrapRXdatabase, [sceneCollection, paragraphCollection], projectId);
+      const replicator2 = new HttpReplicator(oneWrapRXdatabase, [sceneCollection], projectId);
+      isOnline && replicator.startReplicationPull();
+      isOnline && replicator2.startReplicationPush();
+    }
 
+    if (oneWrapRXdatabase && isOnline && startReplication) {
       initializeReplication();
+      setTimeout(() => setStartReplication(false), 5000); // 5 seconds
+    }
 
-  }, [oneWrapRXdatabase, offlineScenes]);
+  }, [oneWrapRXdatabase, isOnline, startReplication]);
 
   return (
-    <DatabaseContext.Provider value={{ oneWrapDb: oneWrapRXdatabase, offlineScenes }}>
+    <DatabaseContext.Provider value={{ oneWrapDb: oneWrapRXdatabase, offlineScenes, setStartReplication }}>
       {children}
     </DatabaseContext.Provider>
   );
