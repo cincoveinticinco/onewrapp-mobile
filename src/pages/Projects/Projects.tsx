402e18aefdbaf91e00160b171fdeee365663cfd9
carreraprogrammer <carreraprogrammer@gmail.com>
2024-04-18 11:04:02 -0500

    Update Projects component to include authentication check

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,34 +2,35 @@ import React from 'react';
 import {
   IonContent, IonHeader, IonPage, IonGrid, IonRow, IonCol,
 } from '@ionic/react';
-// import { useAuth } from '../../context/auth';
+import { useAuth } from '../../context/auth';
 import projects from '../../data';
 import ProjectCard from '../../components/Projects/ProjectCard';
 import Toolbar from '../../components/Shared/Toolbar/Toolbar';
+import { Redirect } from 'react-router';
 
-// const { loggedIn } = useAuth();
+const Projects: React.FC = () => {
+  const { loggedIn } = useAuth();
 
-// if (!loggedIn) {
-//   return <Redirect to="/login" />;
-// }
+  if (!loggedIn) {
+    return <Redirect to="/login" />;
+  }
 
-const Projects: React.FC = () => (
-
-  <IonPage>
-    <IonHeader>
-      <Toolbar name="PROJECTS" search menu />
-    </IonHeader>
-    <IonContent className="ion-padding" color="tertiary">
-      <IonGrid>
-        <IonRow>
-          {projects.map((project) => (
-            <IonCol size-xs="6" size-md="3" size-lg="2" size-sm="3" key={project.id} class="ion-no-padding">
-              <ProjectCard project={project} />
-            </IonCol>
-          ))}
-        </IonRow>
-      </IonGrid>
-    </IonContent>
-  </IonPage>
-);
+  return (
+    <IonPage>
+      <IonHeader>
+        <Toolbar name="PROJECTS" search menu />
+      </IonHeader>
+      <IonContent className="ion-padding" color="tertiary">
+        <IonGrid>
+          <IonRow>
+            {projects.map((project) => (
+              <IonCol size-xs="6" size-md="3" size-lg="2" size-sm="3" key={project.id} class="ion-no-padding">
+                <ProjectCard project={project} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  )};
 export default Projects;
