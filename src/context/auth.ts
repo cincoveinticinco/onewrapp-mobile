5e590125ff73ccba832760af0c3266861bc6d914
carreraprogrammer <carreraprogrammer@gmail.com>
2024-04-18 17:50:24 -0500

    Update AuthContext in auth.ts to include user object

--- src/context/auth.ts
+++ src/context/auth.ts
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-export const AuthContext = React.createContext({ loggedIn: false });
+export const AuthContext = React.createContext({ loggedIn: false, user: null });
 
 export function useAuth() {
   return useContext(AuthContext);
