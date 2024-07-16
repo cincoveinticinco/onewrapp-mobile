a2df7ca68dd79240c71c122247aa3dce87bf1d33
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-16 16:38:51 -0500

    refactor: Add AdvanceCallInfo component to ShootingDetail

---
+++ src/components/ShootingDetail/AdvanceCallInfo/AdvanceCallInfo.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+import { VscEdit } from 'react-icons/vsc';
+import DeleteButton from '../../Shared/DeleteButton/DeleteButton';
+import EditionModal from '../../Shared/EditionModal/EditionModal';
+import { AdvanceCall } from '../../../interfaces/shootingTypes';
+
+interface AdvanceCallInfoProps {
+  call: any;
+  editMode: boolean;
+  getHourMinutesFomISO: (iso: string) => string;
+  deleteAdvanceCall: (call: any) => void;
+  editionInputs: any
+  handleEdition: any
+}
+
+const AdvanceCallInfo: React.FC<AdvanceCallInfoProps> = ({ call, editMode, getHourMinutesFomISO, deleteAdvanceCall, editionInputs, handleEdition }) => {
+  const editionModalRef = React.useRef<HTMLIonModalElement>(null);
+
+  const formatDefaultValues = (call: AdvanceCall) => {
+    return {
+      ...call,
+      adv_call_time: getHourMinutesFomISO(call.adv_call_time)
+    }
+  } 
+
+  const EditModal = () => (
+    <EditionModal
+      modalRef={editionModalRef}
+      modalTrigger={'open-edit-advance-call-modal' + '-' + call.id}
+      title='Add New Advance Call'
+      formInputs={editionInputs}
+      handleEdition={handleEdition}
+      defaultFormValues={{...formatDefaultValues(call)}}
+      modalId={'add-new-advance-call-modal' + '-' + call.id}
+    />
+  )
+
+  const openModal = () => {
+    if (editionModalRef.current) {
+      editionModalRef.current.present();
+    }
+  }
+  return (
+    <>
+      <div 
+        className="ion-padding-start" 
+        style={editMode ? {width: '600px', display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'} : {width: '400px', display: 'grid', gridTemplateColumns: '1fr 1fr'}}
+      >
+        <p><b>{call.dep_name_eng && call.dep_name_eng.toUpperCase()  || call.dep_name_esp && call.dep_name_esp.toUpperCase() || 'NO DEPARTMENT'}: </b></p>
+        <p>{getHourMinutesFomISO(call.adv_call_time)}</p>
+        {
+          editMode &&
+          <div>
+            <IonButton fill="clear" slot="end" color="light" className="toolbar-button" onClick={() => openModal()}>
+              <VscEdit className="toolbar-icon" style={{color: 'var(--ion-color-primary)'}} />
+            </IonButton> 
+            <DeleteButton onClick={() => deleteAdvanceCall(call)} />
+          </div>
+        }
+      </div>
+      <EditModal />
+    </>
+  );
+};
+
+export default AdvanceCallInfo;
