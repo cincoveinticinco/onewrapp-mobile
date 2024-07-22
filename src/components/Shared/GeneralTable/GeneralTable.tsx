617db0de973386beed638b6a403a14e41b806dd2
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-22 18:09:40 -0500

    refactor: Update GeneralTable component to support editing of hour values

--- src/components/Shared/GeneralTable/GeneralTable.tsx
+++ src/components/Shared/GeneralTable/GeneralTable.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
-import { IonContent } from '@ionic/react';
 import './GeneralTable.css';
+import getHourMinutesFomISO from '../../../utils/getHoursMinutesFromISO';
+import { IonInput } from '@ionic/react';
+import zIndex from '@mui/material/styles/zIndex';
 
-interface Column {
+export interface Column {
   key: string;
   title: string;
   sticky?: boolean;
+  type?: 'text' | 'hour';
+  textAlign?: 'left' | 'center' | 'right';
 }
 
 interface GeneralTableProps {
   columns: Column[];
   data: any[];
   stickyColumnCount?: number;
+  editMode?: boolean;
 }
 
-const GeneralTable: React.FC<GeneralTableProps> = ({ columns, data, stickyColumnCount = 1 }) => {
+const GeneralTable: React.FC<GeneralTableProps> = ({ columns, data, stickyColumnCount = 1, editMode = false }) => {
+  const getColumnValue = (row: any, column: Column) => {
+    if(!editMode) {
+      if (column.type === 'hour') {
+        return getHourMinutesFomISO(row[column.key]);
+      }
+    } else {
+      if (column.type === 'hour') {
+      return (
+        <IonInput
+          type="time" 
+          value={getHourMinutesFomISO(row[column.key])}
+          onIonChange={(e) => row[column.key] = e.detail.value}
+          style={{width: 'auto'}}
+        />)
+      }
+    }
+    return row[column.key] && row[column.key].toString().toUpperCase() || '--';
+  }
+
   return (
       <div className="table-container">
         <table className="custom-table">
@@ -26,7 +50,7 @@ const GeneralTable: React.FC<GeneralTableProps> = ({ columns, data, stickyColumn
                   className={index < stickyColumnCount ? 'sticky-column' : ''}
                   style={{left: `${index * 150}px`}}
                 >
-                  {column.title}
+                  {column.title.toUpperCase()}
                 </th>
               ))}
             </tr>
@@ -38,9 +62,9 @@ const GeneralTable: React.FC<GeneralTableProps> = ({ columns, data, stickyColumn
                   <td 
                     key={`${rowIndex}-${column.key}`}
                     className={colIndex < stickyColumnCount ? 'sticky-column' : ''}
-                    style={{left: `${colIndex * 150}px`}}
+                    style={{left: `${colIndex * 150}px`, textAlign: column.textAlign || 'center' }}
                   >
-                    {row[column.key]}
+                    {getColumnValue(row, column)}
                   </td>
                 ))}
               </tr>
