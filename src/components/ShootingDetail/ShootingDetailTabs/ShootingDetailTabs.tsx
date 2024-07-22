bf4e6bdc9bf08dc622273a6e55cb60f171226836
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-22 08:18:06 -0500

    refactor: Add shooting detail tab for managing shooting views

--- src/components/ShootingDetail/ShootingDetailTabs/ShootingDetailTabs.tsx
+++ src/components/ShootingDetail/ShootingDetailTabs/ShootingDetailTabs.tsx
@@ -5,45 +5,66 @@ import { documentTextOutline, informationCircleOutline, serverOutline } from 'io
 import React from 'react';
 import { ShootingViews } from '../../../pages/ShootingDetail/ShootingDetail';
 import './ShootingDetailTabs.css';
+import { useHistory, useParams } from 'react-router';
 
 interface ShootingDetailTabsProps {
   setView: (view: ShootingViews) => void;
   view: ShootingViews;
 }
 
-const ShootingDetailTabs: React.FC<ShootingDetailTabsProps> = ({ setView, view }) => (
-  <IonTabBar slot="bottom" color="dark" mode="md">
-    <IonTabButton
-      tab="shootingScenes"
-      className="tab-bar-buttons"
-      onClick={() => setView('scenes')}
-    >
-      <IonIcon icon={serverOutline} color={view === 'scenes' ? 'primary' : 'light'} />
-      <IonLabel style={
-          {
-            color: view === 'scenes' ? 'var(--ion-color-primary)' : 'var(--ion-color-light)',
-          }
-        }
+const ShootingDetailTabs: React.FC<ShootingDetailTabsProps> = ({ setView, view }) => {
+  const { shootingId } = useParams<{ shootingId: string }>();
+  const { id } = useParams<{ id: string }>();
+  const history = useHistory();
+
+  const goToCallSheet = () => {
+    history.push(`/my/projects/${id}/shooting/${shootingId}/callsheet`);
+  }
+
+  return (
+    <IonTabBar slot="bottom" color="dark" mode="md">
+      <IonTabButton
+        tab="shootingScenes"
+        className="tab-bar-buttons"
+        onClick={() => setView('scenes')}
       >
-        SCENES
-      </IonLabel>
-    </IonTabButton>
-    <IonTabButton
-      tab="shootingInfo"
-      className="tab-bar-buttons"
-      onClick={() => setView('info')}
-    >
-      <IonIcon icon={informationCircleOutline} className="tab-bar-icons" color={view === 'info' ? 'primary' : 'light'} />
-      <IonLabel style={
-          {
-            color: view === 'info' ? 'var(--ion-color-primary)' : 'var(--ion-color-light)',
+        <IonIcon icon={serverOutline} color={view === 'scenes' ? 'primary' : 'light'} />
+        <IonLabel style={
+            {
+              color: view === 'scenes' ? 'var(--ion-color-primary)' : 'var(--ion-color-light)',
+            }
           }
-        }
+        >
+          SCENES
+        </IonLabel>
+      </IonTabButton>
+      <IonTabButton
+        tab="shootingInfo"
+        className="tab-bar-buttons"
+        onClick={() => setView('info')}
       >
-        DAY INFO
-      </IonLabel>
-    </IonTabButton>
-  </IonTabBar>
-);
+        <IonIcon icon={informationCircleOutline} className="tab-bar-icons" color={view === 'info' ? 'primary' : 'light'} />
+        <IonLabel style={
+            {
+              color: view === 'info' ? 'var(--ion-color-primary)' : 'var(--ion-color-light)',
+            }
+          }
+        >
+          DAY INFO
+        </IonLabel>
+      </IonTabButton>
+      <IonTabButton
+        tab="shootingCallTime"
+        className="tab-bar-buttons"
+        onClick={goToCallSheet}
+      >
+        <IonIcon icon={documentTextOutline} className="tab-bar-icons" />
+        <IonLabel>
+          Call Time
+        </IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  );
+}
 
 export default ShootingDetailTabs;
