2eb44583fecfdbd622a9852ddf54911221064573
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-23 18:08:43 -0500

    refactor: Update CastView component to use consistent naming conventions for time fields

--- src/components/CallSheet/CastView/CastView/CastView.tsx
+++ src/components/CallSheet/CastView/CastView/CastView.tsx
@@ -2,29 +2,26 @@ import { useRef } from 'react'
 import EditionModal, { FormInput } from '../../../Shared/EditionModal/EditionModal'
 import GeneralTable, { Column } from '../../../Shared/GeneralTable/GeneralTable'
 import { useParams } from 'react-router'
-import OutlineLightButton from '../../../Shared/OutlineLightButton/OutlineLightButton'
 import NoRegisters from '../../NoRegisters/NoRegisters'
 
-const CastView = ({ castData, addNewModalIsOpen, setIsOpen, editMode }: { castData: any, addNewModalIsOpen: boolean, setIsOpen: any, editMode: any }) => {
+const CastView = ({ castData, addNewModalIsOpen, setIsOpen, editMode, addNewCastCall, castOptions }: { castData: any, addNewModalIsOpen: boolean, setIsOpen: any, editMode: any, addNewCastCall: any, castOptions: any }) => {
 
   const columns = [
     { key: 'cast', title: 'CAST', type: 'text', textAlign: 'left' },
-    { key: 'talent', title: 'TALENT', type: 'text' },
+    { key: 'name', title: 'TALENT', type: 'text' },
     { key: 'tScn', title: 'T. SCN.', type: 'text'},
-    { key: 'pickup', title: 'PICKUP', type: 'hour' },
-    { key: 'call', title: 'CALL', type: 'hour' },
-    { key: 'makeup', title: 'MAKEUP', type: 'hour' },
-    { key: 'wardrobe', title: 'WARDROBE', type: 'hour'},
-    { key: 'ready', title: 'READY', type: 'hour'},
+    { key: 'pickUp', title: 'PICKUP', type: 'hour' },
+    { key: 'callTime', title: 'CALL', type: 'hour' },
+    { key: 'onMakeUp', title: 'MAKEUP', type: 'hour' },
+    { key: 'onWardrobe', title: 'WARDROBE', type: 'hour'},
+    { key: 'readyToShoot', title: 'READY', type: 'hour'},
     { key: 'notes', title: 'NOTES', type: 'text'},
   ] as Column[]
 
   const AddCastCallModal = () => {
     const modalRef = useRef<HTMLIonModalElement>(null)
-    const castOptions = castData.map((cast: any) => ({ value: cast.cast, label: cast.cast }))
     const { shootingId } = useParams<{ shootingId: string }>()
-
-    // Necessary fields: Cast* (select), pickup, call, makeup, wardrobe, ready, notes
+  
     const castCallInputs: FormInput[] = [
       {
         fieldName: 'cast',
@@ -33,47 +30,47 @@ const CastView = ({ castData, addNewModalIsOpen, setIsOpen, editMode }: { castDa
         type: 'select',
         required: true,
         selectOptions: castOptions,
-        col: '4'
+        col: '6'
       },
       {
-        fieldName: 'pickup',
-        label: 'Pickup',
-        placeholder: 'Enter pickup time',
+        fieldName: 'pickUp',
+        label: 'Pick Up',
+        placeholder: 'Enter pick up time',
         type: 'time',
-        required: true,
-        col: '4'
+        required: false,
+        col: '6'
       },
       {
-        fieldName: 'call',
-        label: 'Call',
+        fieldName: 'callTime',
+        label: 'Call Time',
         placeholder: 'Enter call time',
         type: 'time',
         required: true,
-        col: '4'
+        col: '3'
       },
       {
-        fieldName: 'makeup',
-        label: 'Makeup',
-        placeholder: 'Enter makeup time',
+        fieldName: 'onMakeUp',
+        label: 'Make Up',
+        placeholder: 'Enter make up time',
         type: 'time',
-        required: true,
-        col: '4'
+        required: false,
+        col: '3'
       },
       {
-        fieldName: 'wardrobe',
+        fieldName: 'onWardrobe',
         label: 'Wardrobe',
         placeholder: 'Enter wardrobe time',
         type: 'time',
-        required: true,
-        col: '4'
+        required: false,
+        col: '3'
       },
       {
-        fieldName: 'ready',
-        label: 'Ready',
+        fieldName: 'readyToShoot',
+        label: 'Ready to Shoot',
         placeholder: 'Enter ready time',
         type: 'time',
-        required: true,
-        col: '4'
+        required: false,
+        col: '3'
       },
       {
         fieldName: 'notes',
@@ -81,15 +78,15 @@ const CastView = ({ castData, addNewModalIsOpen, setIsOpen, editMode }: { castDa
         placeholder: 'Enter notes',
         type: 'text',
         required: false,
-        col: '12'
+        col: '6'
       }
     ];
-
+  
     return (
       <EditionModal
         title="Add Cast Call"
         formInputs={castCallInputs}
-        handleEdition={(values: any) => console.log(values)}
+        handleEdition={addNewCastCall}
         modalRef={modalRef}
         modalId={`add-cast-call-modal-${shootingId}`}
         modalTrigger='Add Cast Call'
@@ -97,12 +94,11 @@ const CastView = ({ castData, addNewModalIsOpen, setIsOpen, editMode }: { castDa
         isOpen={addNewModalIsOpen}
         setIsOpen={setIsOpen}
       />
-
     )
   }
 
   if(addNewModalIsOpen) return <AddCastCallModal />
-  
+
   if (!castData || castData.length === 0) {
     return (
       <NoRegisters addNew={() => setIsOpen(true)} />
