08838c3672f8e878efca84c7412a31bc7f78dd9c
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-24 18:38:38 -0500

    refactor: Add logout functionality to Settings page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -2,18 +2,38 @@ import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
 } from '@ionic/react';
 import ExploreContainer from '../../components/Shared/ExploreContainer/ExploreContainer';
+import OutlineLightButton from '../../components/Shared/OutlineLightButton/OutlineLightButton';
+import OutlinePrimaryButton from '../../components/Shared/OutlinePrimaryButton/OutlinePrimaryButton';
+import { useAuth } from '../../context/Auth';
+import { useHistory } from 'react-router';
 
-const Settings: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar color="tertiary">
-        <IonTitle>SETTINGS</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent color="tertiary" fullscreen>
-      <ExploreContainer name="Settings page" />
-    </IonContent>
-  </IonPage>
-);
+const Settings: React.FC = () => {
+  const { logout } = useAuth();
+
+  const history = useHistory();
+  
+  const handleLogout = () => {
+    logout();
+    history.push('/login');
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonTitle>SETTINGS</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent color="tertiary" fullscreen>
+        <OutlinePrimaryButton buttonName='LOG OUT' onClick={handleLogout} style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }} />
+      </IonContent>
+    </IonPage>
+  );
+}
 
 export default Settings;
