d9eb1934b5a65c38f389850b5fa0e29c08333358
Daniel <Daniel.Carrera@nbcuni.com>
2024-07-24 06:44:54 -0500

    refactor: Update ExtraView component to include editExtraCall function

--- src/components/CallSheet/ExtraView/ExtraView.tsx
+++ src/components/CallSheet/ExtraView/ExtraView.tsx
@@ -10,22 +10,23 @@ interface ExtraViewProps {
   addNewModalIsOpen: boolean;
   setAddNewModalIsOpen: (isOpen: boolean) => void;
   addNewExtraCall: (extraCall: ExtraCall) => void;
+  editExtraCall: (index: number, key: any, newValue: any, type: string) => void;
 }
 
 const columns: Column[] = [
   { key: 'extraName', title: 'Extra', textAlign: 'left' },
   { key: 'talentAgency', title: 'Talent/Agency' },
-  { key: 'quantity', title: 'Quantity' },
-  { key: 'callTime', title: 'Call', type: 'hour' },
-  { key: 'onMakeUp', title: 'Makeup', type: 'hour' },
-  { key: 'onWardrobe', title: 'Wardrobe', type: 'hour' },
-  { key: 'readyToShoot', title: 'Ready', type: 'hour' },
-  { key: 'arrived', title: 'Arrived', type: 'hour' },
-  { key: 'wrap', title: 'Wrap', type: 'hour' },
-  { key: 'notes', title: 'Notes' }
+  { key: 'quantity', title: 'Quantity', type: 'number', editable: true },
+  { key: 'callTime', title: 'Call', type: 'hour', editable: true },
+  { key: 'onMakeUp', title: 'Makeup', type: 'hour', editable: true },
+  { key: 'onWardrobe', title: 'Wardrobe', type: 'hour', editable: true },
+  { key: 'readyToShoot', title: 'Ready', type: 'hour', editable: true },
+  { key: 'arrived', title: 'Arrived', type: 'hour', editable: true },
+  { key: 'wrap', title: 'Wrap', type: 'hour', editable: true },
+  { key: 'notes', title: 'Notes', editable: true }
 ];
 
-const ExtraView: React.FC<ExtraViewProps> = ({ extraViewData, editMode, addNewModalIsOpen, setAddNewModalIsOpen, addNewExtraCall }) => {
+const ExtraView: React.FC<ExtraViewProps> = ({ extraViewData, editMode, addNewModalIsOpen, setAddNewModalIsOpen, addNewExtraCall, editExtraCall }) => {
   const modalRef = React.useRef<HTMLIonModalElement>(null);
 
   // Para el modal de creacion, necesito las opciones Extra, Quantity, CAll, makeup, wardrobe, ready, arrived, wrap, notes y talent agency. Las llaves respectivas son extraName, quantity, callTime, onMakeUp, onWardrobe, readyToShoot, arrived, wrap, notes y talentAgency
@@ -118,7 +119,7 @@ const ExtraView: React.FC<ExtraViewProps> = ({ extraViewData, editMode, addNewMo
   if(!extraViewData.length) return <NoRegisters addNew={() => setAddNewModalIsOpen(true)} />
 
   return (
-    <GeneralTable columns={columns} data={extraViewData} editMode={editMode} />
+    <GeneralTable columns={columns} data={extraViewData} editMode={editMode} editFunction={editExtraCall} />
   );
 };
 
