d52a1005c05eee6ed72a3da487dfb2af8d0332e0
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-01 11:31:50 -0500

    feat: Add Crew interface for managing crew members

---
+++ src/interfaces/crewTypes.ts
@@ -0,0 +1,14 @@
+export interface Crew {
+  id: string;
+  depNameEng: string;
+  depNameEsp: string;
+  positionEsp: string;
+  positionEng: string;
+  projectId: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  updatedAt: string;
+  unitNumber: number;
+  departmentId: string;
+}
