810d831c41c729ff263f8f4f01a7d0e55782114e
carreraprogrammer <carreraprogrammer@gmail.com>
2024-09-20 12:33:10 -0500

    Refactor package.json and package-lock.json to increase NODE_OPTIONS max-old-space-size to 16384 during build

--- src/templates/MinimalTemplate.ts
+++ src/templates/MinimalTemplate.ts
@@ -1,29 +1,29 @@
-import { Template, BLANK_PDF } from '@pdfme/common';
+// import { Template, BLANK_PDF } from '@pdfme/common';
 
-const template: Template = {
-  basePdf: BLANK_PDF,
-  schemas: [
-    {
-      a: {
-        type: 'text',
-        position: { x: 16, y: 20 },
-        width: 200,
-        height: 10,
-      },
-      b: {
-        type: 'text',
-        position: { x: 16, y: 30 },
-        width: 200,
-        height: 10,
-      },
-      c: {
-        type: 'text',
-        position: { x: 16, y: 40 },
-        width: 200,
-        height: 10,
-      },
-    },
-  ],
+const template= {
+  // basePdf: BLANK_PDF,
+  // schemas: [
+  //   {
+  //     a: {
+  //       type: 'text', 
+  //       position: { x: 16, y: 20 },
+  //       width: 200,
+  //       height: 10,
+  //     },
+  //     b: {
+  //       type: 'text',
+  //       position: { x: 16, y: 30 },
+  //       width: 200,
+  //       height: 10,
+  //     },
+  //     c: {
+  //       type: 'text',
+  //       position: { x: 16, y: 40 },
+  //       width: 200,
+  //       height: 10,
+  //     },
+  //   },
+  // ],
 };
 
 export default template;
