97af1c66b185f3f40ad3438f06f26d65ec4d24a9
carreraprogrammer <carreraprogrammer@gmail.com>
2024-09-27 14:41:16 -0500

    Remove GoogleMaps import and update GoogleAuth scopes

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Capacitor
-import GoogleMaps
-import GoogleMapsUtils
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -10,7 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyA9rz3ykn0BboO4tjO2_fSKE82_CiIZmJI")
         return true
     }
 
