26c7eebd3fb29b2f942184c77331a1e1ce5fba6a
carreraprogrammer <carreraprogrammer@gmail.com>
2025-03-20 09:38:08 -0500

    Add stripboardName prop to WeeksList and integrate ModalToolbar for enhanced UI

--- src/pages/StripBoard/components/StripboardModal/Components/WeeksList/WeeksList.tsx
+++ src/pages/StripBoard/components/StripboardModal/Components/WeeksList/WeeksList.tsx
@@ -1,18 +1,26 @@
-import { IonContent } from "@ionic/react";
+import { IonContent, IonHeader } from "@ionic/react";
 import { StripboardWeeks } from "../../../../../../hooks/database/useStripboards/useStripboards";
 import { SceneDocType } from "../../../../../../Shared/types/scenes.types";
 import WeekItem from "../WeekItem/WeekItem";
+import ModalToolbar from "../../../../../../Shared/Components/modals/ModalToolbar/ModalToolbar";
 
 
 interface WeeksListProps {
   weeks: StripboardWeeks[];
   unitScenes: Record<string, SceneDocType[]>;
   updateUnitScenes: (unitId: number, scenes: SceneDocType[]) => void;
+  stripboardName: string;
 }
 
-const WeeksList: React.FC<WeeksListProps> = ({ weeks, unitScenes, updateUnitScenes }) => {
+const WeeksList: React.FC<WeeksListProps> = ({ weeks, unitScenes, updateUnitScenes, stripboardName }) => {
   return (
     <IonContent color="tertiary" scrollEvents={true} className="hide-scrollbar">
+      <IonHeader>
+        <ModalToolbar
+          toolbarTitle={stripboardName}
+          color="tertiary-dark"
+        />
+      </IonHeader>
       {weeks.map((week, weekIndex) => (
         <WeekItem
           key={`week-${week.weekNumber}-${weekIndex}`} 
